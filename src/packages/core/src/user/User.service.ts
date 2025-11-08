58584820af846f5caea36d826a4ed6e8a2bb950a
nicosammito <nico@sammito.de>
2025-11-08 03:45:14 +0100

    feat: enhance UserService with GraphQL login mutation and additional user methods

--- src/packages/core/src/user/User.service.ts
+++ src/packages/core/src/user/User.service.ts
@@ -1,5 +1,6 @@
-import {DUserReactiveService} from "@code0-tech/pictor";
+import {DUserReactiveService, DUserView, ReactiveArrayStore} from "@code0-tech/pictor";
 import {
+    User,
     UsersEmailVerificationInput,
     UsersEmailVerificationPayload,
     UsersIdentityLinkInput,
@@ -27,44 +28,87 @@ import {
     UsersRegisterInput,
     UsersRegisterPayload
 } from "@code0-tech/sagittarius-graphql-types";
+import {GraphqlClient} from "@core/util/graphql-client";
+import loginMutation from "../user/mutations/User.login.mutation.graphql";
+import logoutMutation from "../user/mutations/User.login.mutation.graphql";
 
 export class UserService extends DUserReactiveService {
+
+    private readonly client: GraphqlClient
+
+    constructor(client: GraphqlClient, store: ReactiveArrayStore<DUserView>) {
+        super(store);
+        this.client = client
+    }
+
+    values(): DUserView[] {
+        return super.values();
+    }
+
+    getById(id: User["id"]): DUserView | undefined {
+        return super.getById(id);
+    }
+
+    /** @alpha **/
     usersEmailVerification(payload: UsersEmailVerificationInput): Promise<UsersEmailVerificationPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersIdentityLink(payload: UsersIdentityLinkInput): Promise<UsersIdentityLinkPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersIdentityLogin(payload: UsersIdentityLoginInput): Promise<UsersIdentityLoginPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersIdentityRegister(payload: UsersIdentityRegisterInput): Promise<UsersIdentityRegisterPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersIdentityUnlink(payload: UsersIdentityUnlinkInput): Promise<UsersIdentityUnlinkPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
-    usersLogin(payload: UsersLoginInput): Promise<UsersLoginPayload | undefined> {
-        return Promise.resolve(undefined);
+    async usersLogin(payload: UsersLoginInput): Promise<UsersLoginPayload | undefined> {
+        const result = await this.client.mutate<UsersLoginPayload, UsersLoginInput>({
+            mutation: loginMutation,
+            variables: {
+                ...payload
+            }
+        })
+
+        //store user session
+        if (result.data && result.data.userSession) {
+            this.createUserSession(result.data.userSession)
+        }
+
+        if (result.data && result.data.userSession?.user) {
+            this.add(new DUserView(result.data.userSession.user))
+        }
+
+        return result.data ?? undefined
     }
 
     usersLogout(payload: UsersLogoutInput): Promise<UsersLogoutPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersMfaBackupCodesRotate(payload: UsersMfaBackupCodesRotateInput): Promise<UsersMfaBackupCodesRotatePayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersMfaTotpGenerateSecret(payload: UsersMfaTotpGenerateSecretInput): Promise<UsersMfaTotpGenerateSecretPayload | undefined> {
         return Promise.resolve(undefined);
     }
 
+    /** @alpha **/
     usersMfaTotpValidateSecret(payload: UsersMfaTotpValidateSecretInput): Promise<UsersMfaTotpValidateSecretPayload | undefined> {
         return Promise.resolve(undefined);
     }
