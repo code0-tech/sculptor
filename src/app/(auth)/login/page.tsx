78b3b3aa44619a1741d520785326dfb988111173
nicosammito <nico@sammito.de>
2025-11-09 02:06:38 +0100

    feat: replace LoginPage with UserLoginPage component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import {UserLoginPage} from "@edition/user/UserLoginPage";
 
-export default function LoginPage() {
-    return <>Login page</>
+export default function Page() {
+    return <UserLoginPage/>
 }
