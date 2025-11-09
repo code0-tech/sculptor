a5f532ad9c9d4f21e7b3ad7ccee609256860c078
nicosammito <nico@sammito.de>
2025-11-09 20:33:14 +0100

    feat: enhance ApplicationLayout to include a bar prop and improve context management

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,16 +1,30 @@
+"use client"
+
 import React from "react";
-import Link from "next/link";
+import {useApolloClient} from "@apollo/client/react";
+import {Container, ContextStoreProvider, DLayout, DUserView, useReactiveArrayService} from "@code0-tech/pictor";
+import {UserService} from "@core/user/User.service";
+import {GraphqlClient} from "@core/util/graphql-client";
 
 interface ApplicationLayoutProps {
-    children: React.ReactNode
-    modal: React.ReactNode
+    children: React.ReactElement
+    modal: React.ReactElement
+    bar: React.ReactElement
 }
 
-const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, modal}) => {
-    return <div>
-        {modal}
-        {children}
-    </div>
+const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, modal, bar}) => {
+
+    const client = useApolloClient()
+    const [store, service] = useReactiveArrayService<DUserView, UserService>((store) => new UserService(new GraphqlClient(client), store))
+
+    return React.useMemo(() => <Container>
+        <ContextStoreProvider services={[[store, service]]}>
+            {modal}
+            <DLayout topContent={bar}>
+                {children}
+            </DLayout>
+        </ContextStoreProvider>
+    </Container>, [client, children, modal, bar])
 }
 
 export default ApplicationLayout
