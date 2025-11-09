a511f7fe6046c8c86a76688d07c2ce9edf2cd618
nicosammito <nico@sammito.de>
2025-11-09 22:31:25 +0100

    feat: wrap layout in DFullScreen component for improved responsiveness

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,15 @@
 "use client"
 
-import {Col, Container, ContextStoreProvider, DUserView, Flex, Text, useReactiveArrayService} from "@code0-tech/pictor";
+import {
+    Col,
+    Container,
+    ContextStoreProvider,
+    DFullScreen,
+    DUserView,
+    Flex,
+    Text,
+    useReactiveArrayService
+} from "@code0-tech/pictor";
 import {UserService} from "@core/user/User.service";
 import {useApolloClient} from "@apollo/client/react";
 import {GraphqlClient} from "@core/util/graphql-client";
@@ -13,34 +22,36 @@ export default function AuthLayout({children}: Readonly<{ children: React.ReactN
     const [store, service] = useReactiveArrayService<DUserView, UserService>((store) => new UserService(new GraphqlClient(client), store))
 
     return (
-        <div style={{
-            backgroundImage: "url(/CodeZero_Rainbow.png)",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center bottom",
-            width: "100%",
-            height: "100%",
-        }}>
-            <ContextStoreProvider services={[[store, service]]}>
-                <Container h={"100%"} w={"100%"}>
-                    <Flex h={"100%"} w={"100%"} align={"center"} justify={"center"}>
-                        <Col xs={4}>
-                            {children}
-                        </Col>
-                    </Flex>
-                </Container>
-            </ContextStoreProvider>
+        <DFullScreen>
             <div style={{
-                position: "fixed",
-                bottom: "1.3rem",
-                right: "1.3rem",
-                display: "flex",
-                alignItems: "center",
-                gap: "1.3rem"
+                backgroundImage: "url(/CodeZero_Rainbow.png)",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center bottom",
+                width: "100%",
+                height: "100%",
             }}>
-                <Text>All rights reserved &copy; Code0 UG (haftungsbeschränkt)</Text>
-                <Image src={"/CodeZero_Banner_Transparent.png"} alt={"CodeZero Banner"} width={150} height={0}
-                       style={{width: '150px', height: 'auto'}}/>
+                <ContextStoreProvider services={[[store, service]]}>
+                    <Container h={"100%"} w={"100%"}>
+                        <Flex h={"100%"} w={"100%"} align={"center"} justify={"center"}>
+                            <Col xs={4}>
+                                {children}
+                            </Col>
+                        </Flex>
+                    </Container>
+                </ContextStoreProvider>
+                <div style={{
+                    position: "fixed",
+                    bottom: "1.3rem",
+                    right: "1.3rem",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "1.3rem"
+                }}>
+                    <Text>All rights reserved &copy; Code0 UG (haftungsbeschränkt)</Text>
+                    <Image src={"/CodeZero_Banner_Transparent.png"} alt={"CodeZero Banner"} width={150} height={0}
+                           style={{width: '150px', height: 'auto'}}/>
+                </div>
             </div>
-        </div>
+        </DFullScreen>
     );
 }
