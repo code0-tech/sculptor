eef5ac795c8e3e862d0503580b39736e963201cf
nicosammito <nico@sammito.de>
2025-11-09 20:33:39 +0100

    feat: update ApplicationPage to display organizations with sorting and creation buttons

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,16 +1,25 @@
 "use client"
 
-import {mutate} from "@core/util/graphql"
-import { useApolloClient } from "@apollo/client/react";
+import {Button, Flex, Text} from "@code0-tech/pictor";
+import {IconBuilding, IconChevronDown, IconPlus} from "@tabler/icons-react";
 
 export default function ApplicationPage() {
 
-  const client = useApolloClient();
-  mutate(client).then(value => {
-    console.log(value)
-  })
-
-  return <>
-    Application page
-  </>
+  return <Flex style={{flexDirection: "column", gap: "1.3rem"}}>
+    <Flex justify={"space-between"} align={"center"}>
+      <Text size={"xl"} hierarchy={"primary"}>
+        All organizations
+      </Text>
+      <Flex style={{gap: ".7rem"}} align={"center"}>
+        <Button>
+          Sort
+          <IconChevronDown size={16}/>
+        </Button>
+        <Button color={"success"}>
+          Create organization
+          <IconPlus size={16}/>
+        </Button>
+      </Flex>
+    </Flex>
+  </Flex>
 }
