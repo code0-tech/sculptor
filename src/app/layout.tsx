b427f87a6d9003237bbf1ab6d492e0690b6a3165
nicosammito <nico@sammito.de>
2025-11-09 02:06:25 +0100

    feat: implement authentication middleware in Apollo Client for user session management

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,39 @@
 "use client";
 
 import {DFullScreen} from "@code0-tech/pictor";
-import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
+import {ApolloClient, ApolloLink, HttpLink, InMemoryCache} from "@apollo/client";
 import {ApolloProvider} from "@apollo/client/react";
+import {UserSession} from "@code0-tech/sagittarius-graphql-types";
+import React from "react";
+import "./global.scss"
+import Image from "next/image";
 
-
-const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: "/graphql",
-    }),
-});
 
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
+
+    const [token, setToken] = React.useState<string | null>(null);
+
+    const authMiddleware = React.useMemo(() => new ApolloLink((operation, forward) => {
+        if (token) {
+            operation.setContext({
+                headers: {
+                    authorization: token
+                }
+            })
+        }
+        return forward(operation);
+    }), [token]);
+
+    React.useEffect(() => {
+        const userSession = JSON.parse(localStorage.getItem("ide_code-zero_session")!!) as UserSession
+        if (userSession && userSession.token) setToken(userSession.token)
+    }, [])
+
+    const client = new ApolloClient({
+        cache: new InMemoryCache(),
+        link: ApolloLink.from([authMiddleware, new HttpLink({uri: "/graphql"})]),
+    });
+
     return (
         <html>
         <body>
