7ec33f1f58f737a4437052d8607670cf3d211d14
nicosammito <nico@sammito.de>
2025-11-11 19:15:59 +0100

    feat: add Content Security Policy and X-Frame-Options headers to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,36 @@ import path from "node:path";
 
 const EDITION = process.env.EDITION ?? "ce";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+`
+
 const nextConfig: NextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: cspHeader.replace(/\n/g, ''),
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "DENY"
+                    }
+                ],
+            },
+        ]
+    },
     env: {
         NEXT_PUBLIC_edition: EDITION,
         NEXT_PUBLIC_pictorVersion: "11"
