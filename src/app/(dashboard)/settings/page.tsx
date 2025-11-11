b0df22778d7551ef8c84455c8448138f95dc25fc
nicosammito <nico@sammito.de>
2025-11-11 14:22:57 +0100

    feat: enhance Page component layout with Card sections for better organization

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import {Badge, Button, DLayout, Flex, InputLabel, Spacing, SwitchInput, Text} from "@code0-tech/pictor";
+import {Badge, Button, Card, DLayout, Flex, InputLabel, Spacing, SwitchInput, Text} from "@code0-tech/pictor";
 import {Tab, TabContent, TabList, TabTrigger} from "@code0-tech/pictor/dist/components/tab/Tab";
 import {IconGavel, IconSettings} from "@tabler/icons-react";
 import * as process from "node:process";
+import CardSection from "@code0-tech/pictor/dist/components/card/CardSection";
 
 export default function Page() {
 
@@ -34,31 +35,38 @@ export default function Page() {
                         <Spacing spacing={"xl"}/>
                         <div style={{borderBottom: "1px solid rgba(255,255,255,.1)"}}/>
                         <Spacing spacing={"xl"}/>
-                        <Flex style={{gap: "1.3rem", flexDirection: "column"}}>
-                            <Flex justify={"space-between"} align={"center"}>
-                                <Flex style={{gap: ".35rem", flexDirection: "column"}}>
-                                    <Text size={"md"} hierarchy={"primary"}>Sculptor version</Text>
-                                    <Text size={"md"} hierarchy={"tertiary"}>Version of this application</Text>
+                        <Card p={1.3}>
+                            <CardSection border>
+                                <Flex justify={"space-between"} align={"center"}>
+                                    <Flex style={{gap: ".35rem", flexDirection: "column"}}>
+                                        <Text size={"md"} hierarchy={"primary"}>Sculptor version</Text>
+                                        <Text size={"md"} hierarchy={"tertiary"}>Version of this application</Text>
+                                    </Flex>
+                                    <Badge color={"info"}>v0.0.0-mvp.1</Badge>
                                 </Flex>
-                                <Badge color={"info"}>v0.0.0-mvp.1</Badge>
-                            </Flex>
-                            <div style={{borderBottom: "1px solid rgba(255,255,255,.1)"}}/>
-                            <Flex justify={"space-between"} align={"center"}>
-                                <Flex style={{gap: ".35rem", flexDirection: "column"}}>
-                                    <Text size={"md"} hierarchy={"primary"}>Pictor version</Text>
-                                    <Text size={"md"} hierarchy={"tertiary"}>Version of the UI component library</Text>
+                            </CardSection>
+
+                            <CardSection border>
+                                <Flex justify={"space-between"} align={"center"}>
+                                    <Flex style={{gap: ".35rem", flexDirection: "column"}}>
+                                        <Text size={"md"} hierarchy={"primary"}>Pictor version</Text>
+                                        <Text size={"md"} hierarchy={"tertiary"}>Version of the UI component
+                                            library</Text>
+                                    </Flex>
+                                    <Badge
+                                        color={"info"}>{process.env.NEXT_PUBLIC_pictorVersion ?? "v0.0.0-mvp.10"}</Badge>
                                 </Flex>
-                                <Badge color={"info"}>{process.env.NEXT_PUBLIC_pictorVersion ?? "v0.0.0-mvp.10"}</Badge>
-                            </Flex>
-                            <div style={{borderBottom: "1px solid rgba(255,255,255,.1)"}}/>
-                            <Flex justify={"space-between"} align={"center"}>
-                                <Flex style={{gap: ".35rem", flexDirection: "column"}}>
-                                    <Text size={"md"} hierarchy={"primary"}>Sagittarius version</Text>
-                                    <Text size={"md"} hierarchy={"tertiary"}>Version of the backend</Text>
+                            </CardSection>
+                            <CardSection border>
+                                <Flex justify={"space-between"} align={"center"}>
+                                    <Flex style={{gap: ".35rem", flexDirection: "column"}}>
+                                        <Text size={"md"} hierarchy={"primary"}>Sagittarius version</Text>
+                                        <Text size={"md"} hierarchy={"tertiary"}>Version of the backend</Text>
+                                    </Flex>
+                                    <Badge color={"info"}>2134929721-ee</Badge>
                                 </Flex>
-                                <Badge color={"info"}>2134929721-ee</Badge>
-                            </Flex>
-                        </Flex>
+                            </CardSection>
+                        </Card>
                     </TabContent>
                     <TabContent value={"restrictions"}>
                         <Text size={"xl"} hierarchy={"primary"}>Restrictions</Text>
@@ -66,23 +74,31 @@ export default function Page() {
                         <div style={{borderBottom: "1px solid rgba(255,255,255,.1)"}}/>
                         <Spacing spacing={"xl"}/>
                         <Flex style={{gap: "1.3rem", flexDirection: "column"}}>
-                            <Flex justify={"space-between"} align={"center"}>
-                                <Flex style={{gap: ".35rem", flexDirection: "column"}}>
-                                    <Text size={"md"} hierarchy={"primary"}>Organization creation</Text>
-                                    <Text size={"md"} hierarchy={"tertiary"}>Set if organization creation is restricted
-                                        to
-                                        administrators.</Text>
+                            <Card p={1.3}>
+                                <Flex justify={"space-between"} align={"center"}>
+                                    <Flex style={{gap: ".35rem", flexDirection: "column"}}>
+                                        <Text size={"md"} hierarchy={"primary"}>Organization creation</Text>
+                                        <Text size={"md"} hierarchy={"tertiary"}>Set if organization creation is
+                                            restricted
+                                            to
+                                            administrators.</Text>
+                                    </Flex>
+                                    <SwitchInput w={"40px"}/>
                                 </Flex>
-                                <SwitchInput w={"40px"}/>
-                            </Flex>
+                            </Card>
+                            <Spacing spacing={"xxs"}/>
+                            <Text size={"xl"} hierarchy={"primary"}>Danger Zone</Text>
                             <div style={{borderBottom: "1px solid rgba(255,255,255,.1)"}}/>
-                            <Flex justify={"space-between"} align={"center"}>
-                                <Flex style={{gap: ".35rem", flexDirection: "column"}}>
-                                    <Text size={"md"} hierarchy={"primary"}>User registration</Text>
-                                    <Text size={"md"} hierarchy={"tertiary"}>Set if user registration is enabled.</Text>
+                            <Card color={"error"} p={1.3}>
+                                <Flex justify={"space-between"} align={"center"}>
+                                    <Flex style={{gap: ".35rem", flexDirection: "column"}}>
+                                        <Text size={"md"} hierarchy={"primary"}>User registration</Text>
+                                        <Text size={"md"} hierarchy={"tertiary"}>Set if user registration is
+                                            enabled.</Text>
+                                    </Flex>
+                                    <SwitchInput w={"40px"}/>
                                 </Flex>
-                                <SwitchInput w={"40px"}/>
-                            </Flex>
+                            </Card>
                         </Flex>
 
                     </TabContent>
