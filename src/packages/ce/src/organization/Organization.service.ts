ac23b3f3bf02e7f888e890b3d272449c9ebfb8d9
nicosammito <nico@sammito.de>
2025-11-17 00:54:57 +0100

    feat: implement organization creation and query functionality in OrganizationService

--- src/packages/ce/src/organization/Organization.service.ts
+++ src/packages/ce/src/organization/Organization.service.ts
@@ -1,16 +1,40 @@
-import {DOrganizationReactiveService, DOrganizationView} from "@code0-tech/pictor";
-import type {
+import {DOrganizationReactiveService, DOrganizationView, ReactiveArrayStore} from "@code0-tech/pictor";
+import {
+    Mutation,
     Organization,
     OrganizationsCreateInput,
     OrganizationsCreatePayload,
-    OrganizationsDeleteInput, OrganizationsDeletePayload
+    OrganizationsDeleteInput,
+    OrganizationsDeletePayload, Query
 } from "@code0-tech/sagittarius-graphql-types";
+import {GraphqlClient} from "@core/util/graphql-client";
+import createOrganizationMutation from "./mutations/Organization.create.mutation.graphql";
+import organizationQuery from "./queries/Organization.query.graphql";
 
 export class OrganizationService extends DOrganizationReactiveService {
 
+    private readonly client: GraphqlClient
+
+    constructor(client: GraphqlClient, store: ReactiveArrayStore<DOrganizationView>) {
+        super(store);
+        this.client = client
+    }
+
     values(): DOrganizationView[] {
-        if (super.values().length > 0) return super.values()
-        return super.values()
+        if (super.values().length > 0) return super.values();
+        this.client.query<Query>({
+            query: organizationQuery
+        }).then(result => {
+            const data = result.data
+            if (!data) return
+
+            if (data.organizations && data.organizations.nodes) {
+                data.organizations.nodes.forEach((organization, index) => {
+                    if (organization) this.set(index, new DOrganizationView(organization))
+                })
+            }
+        })
+        return super.values();
     }
 
     deleteById(id: Organization["id"]): void {
@@ -23,11 +47,25 @@ export class OrganizationService extends DOrganizationReactiveService {
         return organization !== undefined
     }
 
-    organizationCreate(payload: OrganizationsCreateInput): Promise<OrganizationsCreatePayload | undefined> {
-        return Promise.resolve(undefined)
+    async organizationCreate(payload: OrganizationsCreateInput): Promise<OrganizationsCreatePayload | undefined> {
+        const result = await this.client.mutate<Mutation, OrganizationsCreateInput>({
+            mutation: createOrganizationMutation,
+            variables: {
+                ...payload
+            }
+        })
+
+        if (result.data && result.data.organizationsCreate && result.data.organizationsCreate.organization) {
+            const organization = result.data.organizationsCreate.organization
+            if (!this.hasById(organization.id)) {
+                this.add(new DOrganizationView(organization))
+            }
+        }
+
+        return result.data?.organizationsCreate ?? undefined
     }
 
-    organizationDelete(payload: OrganizationsDeleteInput): Promise<OrganizationsDeletePayload | undefined> {
+    async organizationDelete(payload: OrganizationsDeleteInput): Promise<OrganizationsDeletePayload | undefined> {
         return Promise.resolve(undefined)
     }
 
