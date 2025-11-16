1317db2a5ed3560296055ef3c5d190579847c403
nicosammito <nico@sammito.de>
2025-11-16 22:59:26 +0100

    feat: refactor user service initialization in ApplicationLayout component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -23,13 +23,13 @@ interface ApplicationLayoutProps {
 const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, bar, tab}) => {
 
     const client = useApolloClient()
-    const [store, service] = useReactiveArrayService<DUserView, UserService>((store) => new UserService(new GraphqlClient(client), store))
+    const user = useReactiveArrayService<DUserView, UserService>((store) => new UserService(new GraphqlClient(client), store))
     const router = useRouter()
     const currentSession = useUserSession()
 
     if (currentSession === null) router.push("/login")
 
-    return <ContextStoreProvider services={[[store, service]]}>
+    return <ContextStoreProvider services={[user]}>
         <DLayout topContent={
             <>
                 <div style={{background: "rgba(255,2552,255,.1)", borderBottom: "1px solid rgba(255,2552,255,.1)"}}>
