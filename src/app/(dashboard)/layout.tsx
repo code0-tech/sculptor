66355a28ba79cf40f8c3d8001ecdc2af7577032e
nicosammito <nico@sammito.de>
2025-11-17 00:54:36 +0100

    feat: enhance ApplicationLayout with organization, member, and namespace services

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,7 +5,7 @@ import {useApolloClient} from "@apollo/client/react";
 import {
     Container,
     ContextStoreProvider,
-    DLayout,
+    DLayout, DNamespaceMemberView, DNamespaceView, DOrganizationView,
     DUserView,
     useReactiveArrayService,
     useUserSession
@@ -13,6 +13,9 @@ import {
 import {UserService} from "@edition/user/User.service";
 import {GraphqlClient} from "@core/util/graphql-client";
 import {useRouter} from "next/navigation";
+import {OrganizationService} from "@edition/organization/Organization.service";
+import {MemberService} from "@edition/member/Member.service";
+import {NamespaceService} from "@edition/namespace/Namespace.service";
 
 interface ApplicationLayoutProps {
     children: React.ReactNode
@@ -23,13 +26,17 @@ interface ApplicationLayoutProps {
 const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, bar, tab}) => {
 
     const client = useApolloClient()
-    const user = useReactiveArrayService<DUserView, UserService>((store) => new UserService(new GraphqlClient(client), store))
     const router = useRouter()
     const currentSession = useUserSession()
 
+    const user = useReactiveArrayService<DUserView, UserService>((store) => new UserService(new GraphqlClient(client), store))
+    const organization = useReactiveArrayService<DOrganizationView, OrganizationService>((store) => new OrganizationService(new GraphqlClient(client), store))
+    const member = useReactiveArrayService<DNamespaceMemberView, MemberService>((store) => new MemberService(new GraphqlClient(client), store))
+    const namespace = useReactiveArrayService<DNamespaceView, NamespaceService>((store) => new NamespaceService(new GraphqlClient(client), store))
+
     if (currentSession === null) router.push("/login")
 
-    return <ContextStoreProvider services={[user]}>
+    return <ContextStoreProvider services={[user, organization, member, namespace]}>
         <DLayout topContent={
             <>
                 <div style={{background: "rgba(255,2552,255,.1)", borderBottom: "1px solid rgba(255,2552,255,.1)"}}>
@@ -38,7 +45,7 @@ const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, bar, tab
                 </div>
             </>
         }>
-            <Container>
+            <Container h={"100%"} w={"100%"}>
                 {children}
             </Container>
         </DLayout>
