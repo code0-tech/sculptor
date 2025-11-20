ca99f77644582057399bb1303581823fcaaade8d
nicosammito <nico@sammito.de>
2025-11-20 18:04:52 +0100

    feat: add Namespace fragments and service method for fetching by ID

--- src/packages/ce/src/namespace/Namespace.service.ts
+++ src/packages/ce/src/namespace/Namespace.service.ts
@@ -1,5 +1,7 @@
-import {DNamespaceReactiveService, DNamespaceView, ReactiveArrayStore} from "@code0-tech/pictor";
+import {DNamespaceReactiveService, DNamespaceView, DRuntimeView, ReactiveArrayStore} from "@code0-tech/pictor";
 import {GraphqlClient} from "@core/util/graphql-client";
+import {Namespace, Query} from "@code0-tech/sagittarius-graphql-types";
+import namespaceQuery from "./queries/Namespace.query.graphql";
 
 export class NamespaceService extends DNamespaceReactiveService {
 
@@ -8,6 +10,25 @@ export class NamespaceService extends DNamespaceReactiveService {
     constructor(client: GraphqlClient, store: ReactiveArrayStore<DNamespaceView>) {
         super(store);
         this.client = client
+    }
+
+    getById(id: Namespace["id"]): DNamespaceView | undefined {
+        if (super.getById(id)) return super.getById(id);
+        this.client.query<Query>({
+            query: namespaceQuery,
+            variables: {
+                id: id
+            },
+        }).then(result => {
+            const data = result.data
+            if (!data) return
+
+            if (data.namespace) {
+                this.add(new DNamespaceView(data.namespace))
+            }
+        })
+
+        return super.getById(id);
     }
 
 }
