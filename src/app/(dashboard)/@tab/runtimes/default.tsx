2e6fb29aede2652c77b9f5f208e6110cb6048ac3
nicosammito <nico@sammito.de>
2025-11-20 09:56:42 +0100

    feat: create default page component to render ApplicationTabView

---
+++ src/app/(dashboard)/@tab/runtimes/default.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import React from "react";
+import {ApplicationTabView} from "@edition/dashboard/application/ApplicationTabView";
+
+const Page: React.FC = () => {
+    return <ApplicationTabView/>
+}
+
+export default Page
