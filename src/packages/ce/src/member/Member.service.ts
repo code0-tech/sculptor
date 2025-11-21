63da3c5a22a7f3b87f6c35fe73864f8565146651
nicosammito <nico@sammito.de>
2025-11-21 13:09:04 +0100

    feat: enhance MemberService with user-specific member retrieval and additional imports

--- src/packages/ce/src/member/Member.service.ts
+++ src/packages/ce/src/member/Member.service.ts
@@ -1,4 +1,9 @@
-import {DNamespaceMemberReactiveService, DNamespaceMemberView, ReactiveArrayStore} from "@code0-tech/pictor";
+import {
+    DNamespaceMemberReactiveService,
+    DNamespaceMemberView,
+    DRuntimeView,
+    ReactiveArrayStore
+} from "@code0-tech/pictor";
 import {
     Namespace,
     NamespacesMembersAssignRolesInput,
@@ -6,19 +11,52 @@ import {
     NamespacesMembersDeleteInput,
     NamespacesMembersDeletePayload,
     NamespacesMembersInviteInput,
-    NamespacesMembersInvitePayload
+    NamespacesMembersInvitePayload, Query,
+    User
 } from "@code0-tech/sagittarius-graphql-types";
 import {GraphqlClient} from "@core/util/graphql-client";
+import membersQuery from "./queries/Members.query.graphql"
 
 export class MemberService extends DNamespaceMemberReactiveService {
 
     private readonly client: GraphqlClient
     private readonly namespaceId: Namespace['id']
+    private i = 0;
 
     constructor(namespaceId: Namespace['id'], client: GraphqlClient, store: ReactiveArrayStore<DNamespaceMemberView>) {
         super(store);
         this.namespaceId = namespaceId
         this.client = client
+    }
+
+    getByNamespaceIdAndUserId(namespaceId: Namespace["id"], userId: User["id"]): DNamespaceMemberView | undefined {
+        if (super.getByNamespaceIdAndUserId(namespaceId, userId)) return super.getByNamespaceIdAndUserId(namespaceId, userId)
+
+        this.client.query<Query>({
+            query: membersQuery,
+            variables: {
+                namespaceId: namespaceId,
+                firstMember: 50,
+                afterMember: null,
+                firstRole: 50,
+                afterRole: null,
+            }
+        }).then(result => {
+            const data = result.data
+            if (!data) return
+
+            if (data.namespace && data.namespace.members && data.namespace.members.nodes) {
+                data.namespace.members.nodes.forEach((member) => {
+                    if (member) this.set(this.i++, new DNamespaceMemberView(member))
+                })
+            }
+        })
+
+        return super.getByNamespaceIdAndUserId(namespaceId, userId)
+    }
+
+    values(): DNamespaceMemberView[] {
+        return super.values();
     }
 
     memberAssignRoles(payload: NamespacesMembersAssignRolesInput): Promise<NamespacesMembersAssignRolesPayload | undefined> {
