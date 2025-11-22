b6a9cef3607d58a48ae655b839b118bcda7f8e26
nicosammito <nico@sammito.de>
2025-11-23 00:25:48 +0100

    feat: add ApplicationOrganizationView component for organization management

---
+++ src/packages/ce/src/dashboard/application/ApplicationOrganizationView.tsx
@@ -0,0 +1,24 @@
+import {Button, DOrganizationList, Flex, Spacing, Text, TextInput} from "@code0-tech/pictor";
+import {IconSearch} from "@tabler/icons-react";
+import Link from "next/link";
+import React from "react";
+
+export const ApplicationOrganizationView = () => {
+
+    return <>
+        <Flex align={"center"} justify={"space-between"}>
+            <Text size={"xl"} hierarchy={"primary"}>
+                Organizations
+            </Text>
+            <Flex align={"center"} style={{gap: "0.7rem"}}>
+                <TextInput left={<IconSearch size={16}/>} placeholder={"Find an organization..."}/>
+                <Link href={"/organizations/create"}>
+                    <Button color={"success"}>Create Organization</Button>
+                </Link>
+            </Flex>
+        </Flex>
+        <Spacing spacing={"xl"}/>
+        <DOrganizationList/>
+    </>
+
+}
