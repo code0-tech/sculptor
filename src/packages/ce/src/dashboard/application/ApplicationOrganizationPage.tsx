600a15ef618bae2257d0789009b6d714b750411c
nicosammito <nico@sammito.de>
2025-11-23 00:25:45 +0100

    feat: refactor ApplicationOrganizationPage to use ApplicationOrganizationView component

--- src/packages/ce/src/dashboard/application/ApplicationOrganizationPage.tsx
+++ src/packages/ce/src/dashboard/application/ApplicationOrganizationPage.tsx
@@ -1,24 +1,8 @@
 "use client"
 
-import {Button, DOrganizationList, Flex, Spacing, Text, TextInput} from "@code0-tech/pictor";
-import {IconSearch} from "@tabler/icons-react";
 import React from "react";
-import Link from "next/link";
+import {ApplicationOrganizationView} from "@edition/dashboard/application/ApplicationOrganizationView";
 
 export const ApplicationOrganizationPage = () => {
-    return <>
-        <Flex align={"center"} justify={"space-between"}>
-            <Text size={"xl"} hierarchy={"primary"}>
-                Organizations
-            </Text>
-            <Flex align={"center"} style={{gap: "0.7rem"}}>
-                <TextInput left={<IconSearch size={16}/>} placeholder={"Find an organization..."}/>
-                <Link href={"/organizations/create"}>
-                    <Button color={"success"}>Create Organization</Button>
-                </Link>
-            </Flex>
-        </Flex>
-        <Spacing spacing={"xl"}/>
-        <DOrganizationList/>
-    </>
+    return <ApplicationOrganizationView/>
 }
