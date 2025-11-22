d2cae6cd25ebd957921ae66da864d2a74d2d16de
nicosammito <nico@sammito.de>
2025-11-23 00:25:59 +0100

    feat: add ApplicationPersonalProjectView for displaying personal projects

---
+++ src/packages/ce/src/dashboard/application/ApplicationPersonalProjectView.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+    Button,
+    DNamespaceProjectList,
+    Flex, Spacing,
+    Text,
+    TextInput,
+    useService,
+    useStore,
+    useUserSession
+} from "@code0-tech/pictor";
+import {UserService} from "@edition/user/User.service";
+import {IconSearch} from "@tabler/icons-react";
+import Link from "next/link";
+
+export const ApplicationPersonalProjectView: React.FC = () => {
+
+    const userSession = useUserSession()
+    const userStore = useStore(UserService)
+    const userService = useService(UserService)
+
+    const currentUser = React.useMemo(() => userService.getById(userSession?.user?.id), [userStore, userSession])
+
+    const projectsList = React.useMemo(() => {
+        if (!currentUser || !currentUser.namespace) return null
+        return <DNamespaceProjectList namespaceId={currentUser.namespace.id}/>
+
+    }, [currentUser])
+
+    return <>
+
+        <Flex align={"center"} justify={"space-between"}>
+            <Text size={"xl"} hierarchy={"primary"}>
+                Personal projects
+            </Text>
+            <Flex align={"center"} style={{gap: "0.7rem"}}>
+                <TextInput left={<IconSearch size={16}/>} placeholder={"Find a personal project..."}/>
+                <Link href={"/projects/create"}>
+                    <Button color={"success"}>Create personal project</Button>
+                </Link>
+            </Flex>
+        </Flex>
+        <Spacing spacing={"xl"}/>
+        {projectsList}
+
+    </>
+
+}
