88867c9f6cf889802f5b77645fde87296ce7d4b8
nicosammito <nico@sammito.de>
2025-11-24 14:42:57 +0100

    feat: enhance NamespaceTabView with organization settings tab and service integration

--- src/packages/ce/src/dashboard/namespace/NamespaceTabView.tsx
+++ src/packages/ce/src/dashboard/namespace/NamespaceTabView.tsx
@@ -1,23 +1,53 @@
 "use client"
 
 import React from "react";
-import {Button, Container} from "@code0-tech/pictor";
+import {Button, Container, useService, useStore} from "@code0-tech/pictor";
 import {Tab, TabList, TabTrigger} from "@code0-tech/pictor/dist/components/tab/Tab";
 import {IconFolders, IconHome, IconServer, IconSettings, IconUserCog, IconUsers} from "@tabler/icons-react";
 import Link from "next/link";
 import {useParams, usePathname} from "next/navigation";
+import {NamespaceService} from "@edition/namespace/Namespace.service";
+import {OrganizationService} from "@edition/organization/Organization.service";
 
 export const NamespaceTabView: React.FC = () => {
 
     const params = useParams()
     const pathname = usePathname()
-    const baseLink = `/namespace/${params.namespaceId}`
+    const namespaceId = params.namespaceId as any
+
+    const namespaceService = useService(NamespaceService)
+    const namespaceStore = useStore(NamespaceService)
+    const organizationService = useService(OrganizationService)
+    const organizationStore = useStore(OrganizationService)
+
+    const namespace = React.useMemo(() => namespaceService.getById(`gid://sagittarius/Namespace/${namespaceId}`), [namespaceStore, namespaceId])
+    const parentOrganization = React.useMemo(() => namespace?.parent?.__typename === "Organization" ? organizationService.getById(namespace?.parent?.id) : null, [organizationStore, namespace])
+
+    const baseLink = `/namespace/${namespaceId}`
     const defaultValue = pathname.includes("projects") ? "projects"
         : pathname.includes("members") ? "members"
             : pathname.includes("roles") ? "roles"
                 : pathname.includes("runtimes") ? "runtimes"
                     : pathname.includes("settings") ? "settings"
                         : "overview"
+
+    const settings = React.useMemo(() => {
+        return namespace?.parent?.__typename == "Organization"
+        && parentOrganization
+        && (
+            parentOrganization.userAbilities?.deleteOrganization
+            || parentOrganization?.userAbilities?.updateOrganization
+        ) ? (
+            <TabTrigger value={"settings"}>
+                <Link href={`${baseLink}/settings`}>
+                    <Button variant={"none"}>
+                        <IconSettings size={16}/>
+                        Settings
+                    </Button>
+                </Link>
+            </TabTrigger>
+        ) : null
+    }, [namespace, parentOrganization])
 
     return <Container>
         <Tab defaultValue={defaultValue}>
@@ -62,14 +92,7 @@ export const NamespaceTabView: React.FC = () => {
                         </Button>
                     </Link>
                 </TabTrigger>
-                <TabTrigger value={"settings"}>
-                    <Link href={`${baseLink}/settings`}>
-                        <Button variant={"none"}>
-                            <IconSettings size={16}/>
-                            Settings
-                        </Button>
-                    </Link>
-                </TabTrigger>
+                {settings}
             </TabList>
         </Tab>
     </Container>
