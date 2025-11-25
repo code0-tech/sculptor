f03920c20b36bd88b8e5fa62bd77f5ea5f341ef9
nicosammito <nico@sammito.de>
2025-11-25 13:40:57 +0100

    feat: create main page component to render NamespaceProjectsCreatePage

---
+++ src/app/(dashboard)/namespace/[namespaceId]/projects/create/page.tsx
@@ -0,0 +1,5 @@
+import {NamespaceProjectsCreatePage} from "@edition/dashboard/namespace/NamespaceProjectsCreatePage";
+
+export default function Page() {
+    return <NamespaceProjectsCreatePage/>
+}
