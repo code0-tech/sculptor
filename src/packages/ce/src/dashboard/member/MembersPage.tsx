8ceffe19c3356cb9ee8003a4e82a1810419f3c88
nicosammito <nico@sammito.de>
2025-11-27 02:52:36 +0100

    feat: add MembersPage component to render MembersView

---
+++ src/packages/ce/src/dashboard/member/MembersPage.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import {MembersView} from "@edition/dashboard/member/MembersView";
+import React from "react";
+
+export const MembersPage: React.FC = () => {
+    return <MembersView/>
+}
