fe229d153a0e5991718277b2e5c63a1d23531e8e
nicosammito <nico@sammito.de>
2025-11-28 21:43:20 +0100

    feat: integrate role assignment functionality in MembersView

--- src/packages/ce/src/dashboard/member/MembersView.tsx
+++ src/packages/ce/src/dashboard/member/MembersView.tsx
@@ -2,18 +2,29 @@
 
 import React from "react";
 import {useParams} from "next/navigation";
-import {Button, DNamespaceMemberList, Flex, Spacing, Text, TextInput} from "@code0-tech/pictor";
+import {Button, DNamespaceMemberList, Flex, Spacing, Text, TextInput, useService, useStore} from "@code0-tech/pictor";
 import {IconSearch} from "@tabler/icons-react";
 import Link from "next/link";
+import {MemberService} from "@edition/member/Member.service";
 
 export const MembersView: React.FC = () => {
 
     const params = useParams()
     const namespaceId = params.namespaceId as any as number
+    const memberService = useService(MemberService)
+    const memberStore = useStore(MemberService)
+    const [, startTransition] = React.useTransition()
 
     const membersList = React.useMemo(() => {
-        return <DNamespaceMemberList namespaceId={`gid://sagittarius/Namespace/${namespaceId}`}/>
-    }, [namespaceId])
+        return <DNamespaceMemberList onAssignRole={(member, roles) => {
+            startTransition(() => {
+                memberService.memberAssignRoles({
+                    memberId: member.id!,
+                    roleIds: roles.map(r => r.id!)
+                })
+            })
+        }} namespaceId={`gid://sagittarius/Namespace/${namespaceId}`}/>
+    }, [namespaceId, memberStore])
 
     return <>
 
