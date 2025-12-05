25cefd6fbf6bd641d5140acd8727e6978c9a1ede
nicosammito <nico@sammito.de>
2025-12-05 12:35:52 +0100

    feat: add UsageView component for displaying runtime usage information

---
+++ src/packages/ce/src/dashboard/usage/UsageView.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react";
+import {Button, Flex, Spacing, Text} from "@code0-tech/pictor";
+
+export const UsageView: React.FC = () => {
+    return <>
+        <Text size={"xl"} hierarchy={"primary"}>Runtime usage</Text>
+        <Spacing spacing={"xl"}/>
+        <div style={{borderBottom: "1px solid rgba(255,255,255,.1)"}}/>
+        <Spacing spacing={"xl"}/>
+
+    </>
+}
