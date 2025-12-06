f66a6bf3a211ee6847efb3115936f259b645b28b
nicosammito <nico@sammito.de>
2025-12-06 18:22:02 +0100

    feat: add user invitation form with validation in MemberAddPage

--- src/packages/ce/src/dashboard/member/MemberAddPage.tsx
+++ src/packages/ce/src/dashboard/member/MemberAddPage.tsx
@@ -1,7 +1,18 @@
 "use client"
 
 import React from "react";
-import {Button, Col, DUserInput, Flex, Spacing, Text, useService} from "@code0-tech/pictor";
+import {
+    Button,
+    Col,
+    DUserInput,
+    DUserView,
+    Flex,
+    InputSuggestion,
+    Spacing,
+    Text,
+    useForm,
+    useService
+} from "@code0-tech/pictor";
 import {MemberService} from "@edition/member/Member.service";
 import {useParams, useRouter} from "next/navigation";
 import {Namespace} from "@code0-tech/sagittarius-graphql-types";
@@ -17,6 +28,30 @@ export const MemberAddPage: React.FC = () => {
     const namespaceIndex = params?.namespaceId as string
     const namespaceId: Namespace['id'] = `gid://sagittarius/Namespace/${namespaceIndex as unknown as number}`
 
+    const [inputs, validate] = useForm<{ users: null | InputSuggestion[] }>({
+        initialValues: {
+            users: null
+        },
+        validate: {
+            users: (value) => {
+                if (!value) return "Please select at least one user"
+                if (value.length <= 0) return "Please select at least one user"
+                return null
+            }
+        },
+        onSubmit: (values) => {
+            startTransition(async () => {
+                for (const value of values.users!!) {
+                    await memberService.memberInvite({
+                        namespaceId: namespaceId!!,
+                        userId: (value.valueData as DUserView).id!!
+                    })
+                }
+                router.push(`/namespace/${namespaceIndex}/members`)
+            })
+        }
+    })
+
     return <Flex mih={"100%"} miw={"100%"} align={"center"} justify={"center"}>
         <Col xs={4}>
             <Text size={"xl"} hierarchy={"primary"} display={"block"}>
@@ -27,8 +62,11 @@ export const MemberAddPage: React.FC = () => {
                 Global runtimes are shared runtimes that can be used across multiple organizations.
             </Text>
             <Spacing spacing={"xl"}/>
+            {/*@ts-ignore*/}
             <DUserInput title={"Description"}
-                        description={"Provide a simple project description"}/>
+                        description={"Provide a simple project description"}
+                        validationUsesSuggestions
+                        {...inputs.getInputProps("users")}/>
             <Spacing spacing={"xl"}/>
             <Flex style={{gap: "0.35rem"}} justify={"space-between"}>
                 <Link href={"/"}>
@@ -36,7 +74,7 @@ export const MemberAddPage: React.FC = () => {
                         Go back to members
                     </Button>
                 </Link>
-                <Button color={"success"}>
+                <Button color={"success"} onClick={validate}>
                     Add members
                 </Button>
             </Flex>
