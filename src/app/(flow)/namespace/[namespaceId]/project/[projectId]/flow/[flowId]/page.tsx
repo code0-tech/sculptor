e1a8748adc6b935c2fa37fa8d4e080229c785c64
nicosammito <nico@sammito.de>
2025-12-19 16:56:00 +0100

    feat: create new page component with flow selection and background image

---
+++ src/app/(flow)/namespace/[namespaceId]/project/[projectId]/flow/[flowId]/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import {
+    Button,
+    DFlow,
+    DFlowTabs,
+    DLayout,
+    DResizableHandle,
+    DResizablePanel,
+    DResizablePanelGroup,
+    Flex,
+    Text
+} from "@code0-tech/pictor";
+import React from "react";
+import {IconDatabase, IconFile, IconMessageChatbot} from "@tabler/icons-react";
+
+export default function Page() {
+    const [show, setShow] = React.useState(false);
+
+    return <DResizablePanel id={"2"} order={2}>
+        <DLayout rightContent={
+            <Flex p={0.35} style={{flexDirection: "column", gap: "0.7rem"}}>
+                <Button onClick={() => setShow(prevState => !prevState)} variant={"none"} paddingSize={"xs"}>
+                    <IconFile size={16}/>
+                </Button>
+                <Button variant={"none"} paddingSize={"xs"}>
+                    <IconDatabase size={16}/>
+                </Button>
+                <Button variant={"none"} paddingSize={"xs"}>
+                    <IconMessageChatbot size={16}/>
+                </Button>
+            </Flex>
+        } bottomContent={
+            <Flex p={0.35} style={{gap: "0.7rem"}}>
+                <Button variant={"none"} paddingSize={"xs"}>
+                    <Text>Logbook</Text>
+                </Button>
+                <Button variant={"none"} paddingSize={"xs"}>
+                    <Text>Problems</Text>
+                </Button>
+            </Flex>
+        }>
+            <DResizablePanelGroup direction={"horizontal"}>
+                <DResizablePanel id={"2"} order={2}>
+                    <DFlow flowId={"gid://sagittarius/Flow/1"}/>
+                </DResizablePanel>
+                {show && (
+                    <>
+                        <DResizableHandle/>
+                        <DResizablePanel id={"3"} order={3} defaultSize={25}>
+                            <DFlowTabs flowId={"gid://sagittarius/Flow/1"}/>
+                        </DResizablePanel>
+                    </>
+                )}
+            </DResizablePanelGroup>
+        </DLayout>
+    </DResizablePanel>
+}
---
+++ src/app/(flow)/namespace/[namespaceId]/project/[projectId]/page.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import {
+    Button,
+    Col,
+    DResizablePanel,
+    Flex,
+    ScrollArea,
+    ScrollAreaScrollbar, ScrollAreaThumb,
+    ScrollAreaViewport,
+    Spacing,
+    Text
+} from "@code0-tech/pictor";
+import React from "react";
+import Link from "next/link";
+
+export default function Page() {
+
+    const [show, setShow] = React.useState(false);
+
+    return <DResizablePanel id={"2"} order={2}>
+        <ScrollArea h={"100%"} w={"100%"} >
+            <ScrollAreaViewport style={{paddingTop: "40vh"}}>
+                <Flex mih={"100%"} miw={"100%"} pos={"relative"} align={"start"}
+                      justify={"center"}>
+                    <Col xs={4}>
+                        <Text size={"xl"} hierarchy={"primary"}>Create or select a flow</Text>
+                        <Spacing spacing={"xs"}/>
+                        <Text size={"md"} hierarchy={"tertiary"}>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+                            invidunt ut
+                            labore et dolore magna aliquyam erat, sed diam voluptua.
+                        </Text>
+                        <Spacing spacing={"xl"}/>
+                        <Flex style={{gap: "0.7rem"}}>
+                            <Link href={"/"}>
+                                <Button color={"primary"}>
+                                    Go back to project overview
+                                </Button>
+                            </Link>
+                            <Button color={"success"}>
+                                Create new flow
+                            </Button>
+                        </Flex>
+                        <Spacing spacing={"xl"}/>
+                        <Text size={"xl"} hierarchy={"primary"}>Or start from a template</Text>
+                    </Col>
+                </Flex>
+            </ScrollAreaViewport>
+            <ScrollAreaScrollbar orientation={"vertical"}>
+                <ScrollAreaThumb/>
+            </ScrollAreaScrollbar>
+        </ScrollArea>
+        <div style={{
+            backgroundImage: "url(/CodeZero_Rainbow.png)",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center bottom",
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+        }}/>
+    </DResizablePanel>
+}
