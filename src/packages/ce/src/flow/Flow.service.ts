6c019aab735a0aaec49ee9dded644db8abf19803
nicosammito <nico@sammito.de>
2025-12-19 16:55:03 +0100

    feat: implement FlowService with GraphQL client integration for flow management

---
+++ src/packages/ce/src/flow/Flow.service.ts
@@ -0,0 +1,71 @@
+import {DFlowDependencies, DFlowReactiveService, DNamespaceMemberView, ReactiveArrayStore} from "@code0-tech/pictor";
+import {
+    Flow, NamespaceMember,
+    NamespacesProjectsFlowsCreateInput,
+    NamespacesProjectsFlowsCreatePayload,
+    NamespacesProjectsFlowsDeleteInput,
+    NamespacesProjectsFlowsDeletePayload,
+    NamespacesProjectsFlowsUpdateInput,
+    NamespacesProjectsFlowsUpdatePayload, Query
+} from "@code0-tech/sagittarius-graphql-types";
+import {GraphqlClient} from "@core/util/graphql-client";
+import flowsQuery from "@edition/flow/queries/Flows.query.graphql";
+
+export class FlowService extends DFlowReactiveService {
+
+    private readonly client: GraphqlClient
+    private i = 0
+
+    constructor(client: GraphqlClient, store: ReactiveArrayStore<Flow>) {
+        super(store)
+        this.client = client
+    }
+
+    values(dependencies?: DFlowDependencies): Flow[] {
+        const flows = super.values()
+        if (!dependencies?.namespaceId || !dependencies.projectId) return flows
+
+        const namespaceId = dependencies.namespaceId
+        const projectId = dependencies.projectId
+        const filtered = flows.filter(flow => flow)
+
+        if (filtered.length <= 0) {
+            this.client.query<Query>({
+                query: flowsQuery,
+                variables: {
+                    namespaceId: namespaceId,
+                    projectId: projectId,
+                    firstFlow: 50,
+                    afterFlow: null,
+                }
+            }).then(res => {
+                const nodes = res.data?.namespace?.project?.flows?.nodes ?? []
+                nodes.forEach(flow => {
+                    if (flow && !this.hasById(flow.id)) {
+                        this.set(this.i++, flow)
+                    }
+                })
+            })
+        }
+
+        return filtered
+    }
+
+    hasById(id: Flow["id"]): boolean {
+        const flow = super.values().find(f => f.id === id)
+        return flow !== undefined
+    }
+
+    flowCreate(payload: NamespacesProjectsFlowsCreateInput): Promise<NamespacesProjectsFlowsCreatePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    flowDelete(payload: NamespacesProjectsFlowsDeleteInput): Promise<NamespacesProjectsFlowsDeletePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+    flowUpdate(payload: NamespacesProjectsFlowsUpdateInput): Promise<NamespacesProjectsFlowsUpdatePayload | undefined> {
+        return Promise.resolve(undefined);
+    }
+
+}
