550c4a021ed76e903868cea26c225c66ef60196c
nicosammito <nico@sammito.de>
2026-02-12 12:54:21 +0100

    feat: enhance Page with dynamic tab navigation, tooltips, and routing based on URL params

--- src/app/(flow)/@tab/namespace/[namespaceId]/project/[projectId]/default.tsx
+++ src/app/(flow)/@tab/namespace/[namespaceId]/project/[projectId]/default.tsx
@@ -1,23 +1,61 @@
 "use client"
 
 import {Tab, TabList, TabTrigger} from "@code0-tech/pictor/dist/components/tab/Tab";
-import {Button} from "@code0-tech/pictor";
-import {IconBuilding, IconHome} from "@tabler/icons-react";
+import {Button, Text, Tooltip, TooltipContent, TooltipPortal, TooltipTrigger} from "@code0-tech/pictor";
+import {IconBuilding, IconHome, IconSettings} from "@tabler/icons-react";
 import React from "react";
+import {useParams, usePathname, useRouter} from "next/navigation";
+import {hashToColor} from "@code0-tech/pictor/dist/components/d-flow/DFlow.util";
 
 export default function Page() {
-    return <Tab defaultValue={"defaultValue"} orientation={"vertical"}>
+
+
+    const pathname = usePathname()
+    const router = useRouter()
+    const params = useParams()
+    const defaultValue = pathname.includes("flow") ? "flow"
+        : "settings"
+
+    const namespaceId = params.namespaceId as any
+    const projectId = params.projectId as any
+    const flowId = params.flowId as any
+
+    return <Tab defaultValue={defaultValue} orientation={"vertical"}>
         <TabList>
-            <TabTrigger value={"overview"} asChild={true}>
-                <Button variant={"none"} paddingSize={"xs"}>
-                    <IconHome size={16}/>
-                </Button>
-            </TabTrigger>
-            <TabTrigger value={"organizations"} asChild={true}>
-                <Button variant={"none"} paddingSize={"xs"}>
-                    <IconBuilding size={16}/>
-                </Button>
-            </TabTrigger>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <TabTrigger value={"flow"} asChild={true}>
+                        <Button variant={"none"} paddingSize={"xs"} onClick={() => {
+                            if (!flowId) {
+                                router.push(`/namespace/${namespaceId}/project/${projectId}/flow`)
+                            } else {
+                                router.push(`/namespace/${namespaceId}/project/${projectId}/flow/${flowId}`)
+                            }
+                        }}>
+                            <IconHome size={16}/>
+                        </Button>
+                    </TabTrigger>
+                </TooltipTrigger>
+                <TooltipPortal>
+                    <TooltipContent side={"right"} sideOffset={8} color={"primary"}>
+                        <Text>Flow builder</Text>
+                    </TooltipContent>
+                </TooltipPortal>
+            </Tooltip>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <TabTrigger value={"settings"} asChild={true}>
+                        <Button variant={"none"} paddingSize={"xs"} onClick={() => router.push(`/namespace/${namespaceId}/project/${projectId}/settings`)}>
+                            <IconSettings color={hashToColor("settings")} size={16}/>
+                        </Button>
+                    </TabTrigger>
+                </TooltipTrigger>
+                <TooltipPortal>
+                    <TooltipContent side={"right"} sideOffset={8} color={"primary"}>
+                        <Text>Settings</Text>
+                    </TooltipContent>
+                </TooltipPortal>
+            </Tooltip>
         </TabList>
     </Tab>
 }
