56bc353c365afe6b7d243c29e8f3a24c0d0c9a5e
nicosammito <nico@sammito.de>
2026-02-12 12:54:32 +0100

    feat: add ApplicationLayout with resizable panel and FolderView integration

---
+++ src/app/(flow)/namespace/[namespaceId]/project/[projectId]/flow/layout.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import React from "react";
+import {DResizableHandle, DResizablePanel, DResizablePanelGroup} from "@code0-tech/pictor";
+import {FolderView} from "@edition/ui-flow/folder/FolderView";
+
+interface ApplicationLayoutProps {
+    children: React.ReactNode
+    bar: React.ReactNode
+    tab: React.ReactNode
+}
+
+const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, bar, tab}) => {
+
+    return <DResizablePanelGroup orientation={"horizontal"}>
+        <DResizablePanel id={"1"} defaultSize={"25%"}>
+            <FolderView/>
+        </DResizablePanel>
+        <DResizableHandle/>
+        {children}
+    </DResizablePanelGroup>
+}
+
+export default ApplicationLayout
