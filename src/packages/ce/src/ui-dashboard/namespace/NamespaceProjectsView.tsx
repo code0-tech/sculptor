c2056040a6a916e85987572367a2587a67c8fc55
nicosammito <nico@sammito.de>
2026-02-12 19:19:55 +0100

    feat: enhance NamespaceProjectsView with sortable and filterable project data table

--- src/packages/ce/src/ui-dashboard/namespace/NamespaceProjectsView.tsx
+++ src/packages/ce/src/ui-dashboard/namespace/NamespaceProjectsView.tsx
@@ -2,37 +2,143 @@
 
 
 import React from "react";
-import {Button, DNamespaceProjectList, Flex, Spacing, Text} from "@code0-tech/pictor";
+import {
+    Button,
+    DataTableFilterInput,
+    DataTableFilterSuggestionMenu,
+    Flex,
+    Menu,
+    MenuCheckboxItem,
+    MenuContent,
+    MenuItem,
+    MenuPortal,
+    MenuTrigger,
+    Spacing,
+    Text,
+    useService,
+    useStore
+} from "@code0-tech/pictor";
 import Link from "next/link";
 import {useParams, useRouter} from "next/navigation";
+import {ProjectDataTableComponent} from "@edition/ui-dashboard/project/ProjectDataTableComponent";
+import {DataTableFilterProps, DataTableSortProps} from "@code0-tech/pictor/dist/components/data-table/DataTable";
+import {ButtonGroup} from "@code0-tech/pictor/dist/components/button-group/ButtonGroup";
+import {IconCheck, IconMinus, IconSortAscending, IconSortDescending} from "@tabler/icons-react";
+import {ProjectService} from "@edition/project/Project.service";
+import {ProjectDataTableFilterInputComponent} from "@edition/ui-dashboard/project/ProjectDataTableFilterInputComponent";
 
+//TODO: user abilities for project creation within namespace
 export const NamespaceProjectsView: React.FC = () => {
 
     const params = useParams()
     const router = useRouter()
     const namespaceId = params.namespaceId as any as number
 
-    const projectsList = React.useMemo(() => {
-        return <DNamespaceProjectList color={"secondary"} onSelect={(project) => {
-            const number = project.id?.match(/NamespaceProject\/(\d+)$/)?.[1]
-            router.push(`/namespace/${namespaceId}/project/${number}/flow`)
-        }} namespaceId={`gid://sagittarius/Namespace/${namespaceId}`}/>
-    }, [namespaceId])
+    const projectService = useService(ProjectService)
+    const projectStore = useStore(ProjectService)
 
-    //TODO: user abilities for project creation within namespace
+    const projects = React.useMemo(
+        () => projectService.values({namespaceId: `gid://sagittarius/Namespace/${namespaceId}`}),
+        [projectStore, namespaceId]
+    )
+
+    const [filter, setFilter] = React.useState<DataTableFilterProps>({})
+    const [sort, setSort] = React.useState<DataTableSortProps>({})
+
+    const projectsList = React.useMemo(() => {
+        return <ProjectDataTableComponent namespaceId={`gid://sagittarius/Namespace/${namespaceId}`}
+                                          filter={filter}
+                                          sort={sort}
+                                          onSelect={(project) => {
+                                              const number = project?.id?.match(/NamespaceProject\/(\d+)$/)?.[1]
+                                              router.push(`/namespace/${namespaceId}/project/${number}/flow`)
+                                          }}/>
+    }, [namespaceId, filter, sort])
 
     return <>
 
         <Flex align={"center"} justify={"space-between"}>
-            <Text size={"xl"} hierarchy={"primary"}>
-                Projects
-            </Text>
-            <Flex align={"center"} style={{gap: "0.7rem"}}>
+            <Flex style={{gap: "0.35rem", flexDirection: "column"}}>
+                <Text size={"xl"} hierarchy={"primary"}>
+                    Projects
+                </Text>
+                <Text size={"sm"} hierarchy={"tertiary"}>
+                    Manage projects that you belong to. You can create new projects and switch between them.
+                </Text>
+            </Flex>
+            <ButtonGroup>
                 <Link href={`/namespace/${namespaceId}/projects/create`}>
                     <Button color={"success"}>Create project</Button>
                 </Link>
-            </Flex>
+                <Menu>
+                    <MenuTrigger asChild>
+                        <Button color={"secondary"} variant={"filled"}>Sort</Button>
+                    </MenuTrigger>
+                    <MenuPortal>
+                        <MenuContent>
+                            <MenuCheckboxItem
+                                checked={sort["name"] === undefined ? "indeterminate" : sort["name"] === "asc"}
+                                onSelect={event => {
+                                    if (sort["name"] === undefined)
+                                        setSort(prev => ({...prev, name: "asc"}))
+                                    else if (sort["name"] === "asc")
+                                        setSort(prev => ({...prev, name: "desc"}))
+                                    else
+                                        setSort(prev => ({...prev, name: undefined}))
+
+                                    event.preventDefault()
+                                    event.stopPropagation()
+                                }}>
+                                {sort["name"] === undefined ? <IconMinus size={13}/> : sort["name"] === "asc" ?
+                                    <IconSortDescending size={13}/> : <IconSortAscending size={13}/>}
+                                Name
+                            </MenuCheckboxItem>
+                            <MenuCheckboxItem
+                                checked={sort["createdAt"] === undefined ? "indeterminate" : sort["createdAt"] === "asc"}
+                                onSelect={event => {
+                                    if (sort["createdAt"] === undefined)
+                                        setSort(prev => ({...prev, createdAt: "asc"}))
+                                    else if (sort["createdAt"] === "asc")
+                                        setSort(prev => ({...prev, createdAt: "desc"}))
+                                    else
+                                        setSort(prev => ({...prev, createdAt: undefined}))
+
+                                    event.preventDefault()
+                                    event.stopPropagation()
+                                }}>
+                                {sort["createdAt"] === undefined ?
+                                    <IconMinus size={13}/> : sort["createdAt"] === "asc" ?
+                                        <IconSortDescending size={13}/> : <IconSortAscending size={13}/>}
+                                Created At
+                            </MenuCheckboxItem>
+                            <MenuCheckboxItem
+                                checked={sort["updatedAt"] === undefined ? "indeterminate" : sort["updatedAt"] === "asc"}
+                                onSelect={event => {
+                                    if (sort["updatedAt"] === undefined)
+                                        setSort(prev => ({...prev, updatedAt: "asc"}))
+                                    else if (sort["updatedAt"] === "asc")
+                                        setSort(prev => ({...prev, updatedAt: "desc"}))
+                                    else
+                                        setSort(prev => ({...prev, updatedAt: undefined}))
+
+                                    event.preventDefault()
+                                    event.stopPropagation()
+                                }}>
+                                {sort["updatedAt"] === undefined ?
+                                    <IconMinus size={13}/> : sort["updatedAt"] === "asc" ?
+                                        <IconSortDescending size={13}/> : <IconSortAscending size={13}/>}
+                                Updated At
+                            </MenuCheckboxItem>
+                        </MenuContent>
+                    </MenuPortal>
+                </Menu>
+            </ButtonGroup>
         </Flex>
+        <Spacing spacing={"xl"}/>
+        <div style={{width: "100%"}}>
+            <ProjectDataTableFilterInputComponent namespaceId={`gid://sagittarius/Namespace/${namespaceId}`}
+                                                  onChange={filter => setFilter(filter)}/>
+        </div>
         <Spacing spacing={"xl"}/>
         {projectsList}
 
