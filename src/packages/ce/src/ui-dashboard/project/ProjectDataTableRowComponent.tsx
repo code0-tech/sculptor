0cd52eff69e28fb0e6fe21bd374ad348c6b8b2c5
nicosammito <nico@sammito.de>
2026-02-13 18:48:15 +0100

    feat: refine project row layout with improved spacing and avatar sizing in ProjectDataTableRowComponent

--- src/packages/ce/src/ui-dashboard/project/ProjectDataTableRowComponent.tsx
+++ src/packages/ce/src/ui-dashboard/project/ProjectDataTableRowComponent.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import {NamespaceProject} from "@code0-tech/sagittarius-graphql-types";
-import {Avatar, Badge, DataTableColumn, Flex, Text, useService, useStore} from "@code0-tech/pictor";
+import {Avatar, DataTableColumn, Flex, Text, useService, useStore} from "@code0-tech/pictor";
 import {ProjectService} from "@edition/project/Project.service";
 import {RuntimeService} from "@edition/runtime/Runtime.service";
-import {IconBinaryTree, IconServer} from "@tabler/icons-react";
 import {hashToColor} from "@code0-tech/pictor/dist/components/d-flow/DFlow.util";
 import {formatDistanceToNow} from "date-fns";
 
@@ -41,19 +40,19 @@ export const ProjectDataTableRowComponent: React.FC<ProjectDataTableRowComponent
 
     return <>
         <DataTableColumn>
-            <Flex style={{flexDirection: "column", gap: "1rem"}}>
-                <Flex style={{flexDirection: "column", gap: "0.35rem"}}>
-                    <Flex align={"center"} style={{gap: "0.7rem"}}>
-                        <Avatar bg={"transparent"}
-                                color={hashToColor("project")}
-                                identifier={project?.name ?? ""}/>
+            <Flex style={{flexDirection: "column", gap: "1.3rem"}}>
+                <Flex align={"center"} style={{gap: "0.7rem"}}>
+                    <Avatar size={32}
+                            color={hashToColor("project")}
+                            identifier={project?.name ?? ""}/>
+                    <Flex style={{flexDirection: "column", gap: "0.35rem"}}>
                         <Text size={"md"} hierarchy={"primary"}>
                             {project?.name}
                         </Text>
+                        <Text>
+                            {project?.description}
+                        </Text>
                     </Flex>
-                    <Text>
-                        {project?.description}
-                    </Text>
                 </Flex>
                 <Text hierarchy={"tertiary"}>
                     Updated {formatDistanceToNow(project?.updatedAt!)} ago
