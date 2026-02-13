60c1a7c14ed9b83f584db7239b31e05f521e8cfc
nicosammito <nico@sammito.de>
2026-02-13 18:47:50 +0100

    feat: enhance ApplicationPage with user greeting, avatar, and improved layout using Card and ScrollArea components

--- src/packages/ce/src/ui-dashboard/application/ApplicationPage.tsx
+++ src/packages/ce/src/ui-dashboard/application/ApplicationPage.tsx
@@ -1,8 +1,28 @@
 import {PersonalProjectsView} from "@edition/ui-dashboard/project/PersonalProjectsView";
 import {OrganizationsView} from "@edition/ui-dashboard/organization/OrganizationsView";
-import {Spacing} from "@code0-tech/pictor";
+import {
+    Avatar,
+    Badge,
+    Card,
+    Flex,
+    ScrollArea, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport,
+    Spacing,
+    Text,
+    useService,
+    useStore,
+    useUserSession
+} from "@code0-tech/pictor";
+import {UserService} from "@edition/user/User.service";
+import React from "react";
+import {hashToColor} from "@code0-tech/pictor/dist/components/d-flow/DFlow.util";
 
 export const ApplicationPage = () => {
+
+    const userStore = useStore(UserService)
+    const userService = useService(UserService)
+
+    const userSession = useUserSession()
+    const currentUser = React.useMemo(() => userService.getById(userSession?.user?.id), [userStore, userSession])
 
     return <div style={{
         background: "#070514",
@@ -11,10 +31,44 @@ export const ApplicationPage = () => {
         borderTopLeftRadius: "1rem",
         borderTopRightRadius: "1rem"
     }}>
+        <ScrollArea h={"100%"} type={"scroll"}>
+            <ScrollAreaViewport>
+                <Spacing spacing={"xl"}/>
+                <Flex style={{gap: "1.3rem"}} align={"center"} ml={0.5}>
+                    <div style={{position: "relative"}}>
+                        <Avatar size={64}
+                                style={{
+                                    height: "fit-content",
+                                }}
+                                identifier={currentUser?.username ?? ""}/>
+                        <Badge pos={"absolute"} right={"-5px"} bottom={"-10px"}>
+                            <Text size={"md"}>👋</Text>
+                        </Badge>
+                    </div>
+                    <div>
+                        <Text hierarchy={"tertiary"} size={"md"}>Welcome back!</Text>
+                        <Text hierarchy={"primary"} style={{fontSize: "2rem"}}>
+                            Hi, @{currentUser?.username ?? "user"}
+                        </Text>
+                    </div>
 
-        <PersonalProjectsView/>
-        <Spacing spacing={"xl"}/>
-        <OrganizationsView/>
+                </Flex>
+                <Spacing spacing={"xl"}/>
+                <Flex style={{gap: "1.3rem"}} align={"start"}>
+                    <Card>
+                        <PersonalProjectsView/>
+                    </Card>
+                    <Card>
+                        <OrganizationsView/>
+                    </Card>
+                </Flex>
+                <Spacing spacing={"xl"}/>
+            </ScrollAreaViewport>
+            <ScrollAreaScrollbar orientation={"vertical"}>
+                <ScrollAreaThumb/>
+            </ScrollAreaScrollbar>
+        </ScrollArea>
+
 
     </div>
 
