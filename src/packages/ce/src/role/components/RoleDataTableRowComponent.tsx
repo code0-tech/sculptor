f89394e5cd953e2b28e4ce2a88a850e18d39f172
nicosammito <nico@sammito.de>
2026-02-15 00:57:40 +0100

    refactor: replace DNamespaceRolePermissions with RolePermissionComponent in RoleDataTableRowComponent for improved consistency

--- src/packages/ce/src/role/components/RoleDataTableRowComponent.tsx
+++ src/packages/ce/src/role/components/RoleDataTableRowComponent.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import {NamespaceProject, NamespaceRole} from "@code0-tech/sagittarius-graphql-types";
-import {Avatar, DataTableColumn, Flex, Text, useService, useStore} from "@code0-tech/pictor";
-import {ProjectService} from "@edition/project/services/Project.service";
-import {RuntimeService} from "@edition/runtime/services/Runtime.service";
-import {hashToColor} from "@code0-tech/pictor/dist/components/d-flow/DFlow.util";
+import {NamespaceRole} from "@code0-tech/sagittarius-graphql-types";
+import {DataTableColumn, Flex, Text, useService, useStore} from "@code0-tech/pictor";
 import {formatDistanceToNow} from "date-fns";
 import {RoleService} from "@edition/role/services/Role.service";
-import {DNamespaceRolePermissions} from "@code0-tech/pictor/dist/components/d-role/DNamespaceRolePermissions";
+import {RolePermissionComponent} from "@edition/role/components/RolePermissionComponent";
 
 export interface ProjectDataTableRowComponentProps {
     roleId: NamespaceRole['id']
@@ -31,7 +28,7 @@ export const RoleDataTableRowComponent: React.FC<ProjectDataTableRowComponentPro
                     <Text size={"md"} hierarchy={"primary"}>
                         {role?.name}
                     </Text>
-                    <DNamespaceRolePermissions abilities={role?.abilities!}/>
+                    <RolePermissionComponent abilities={role?.abilities!}/>
                 </Flex>
                 <Text hierarchy={"tertiary"}>
                     Updated {formatDistanceToNow(role?.updatedAt!)} ago
