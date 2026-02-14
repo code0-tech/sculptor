fc8f848ddb1553b536a6ef043e31e785b2fd8776
nicosammito <nico@sammito.de>
2026-02-15 00:05:48 +0100

    feat: add sort menu to UsersPage with options for username, createdAt, and updatedAt

--- src/packages/ce/src/user/pages/UsersPage.tsx
+++ src/packages/ce/src/user/pages/UsersPage.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import React from "react";
-import {Button, DUserList, Flex, Spacing, Text, useService, useStore, useUserSession,} from "@code0-tech/pictor";
+import {Button, ButtonGroup, Flex, Spacing, Text, useService, useStore, useUserSession, Menu, MenuTrigger, MenuPortal, MenuContent, MenuCheckboxItem} from "@code0-tech/pictor";
 import {UserService} from "@edition/user/services/User.service";
 import {notFound} from "next/navigation";
 import {UserDataTableComponent} from "@edition/user/components/UserDataTableComponent";
 import {DataTableFilterProps, DataTableSortProps} from "@code0-tech/pictor/dist/components/data-table/DataTable";
-import {RuntimeDataTableFilterInputComponent} from "@edition/runtime/components/RuntimeDataTableFilterInputComponent";
 import {UserDataTableFilterInputComponent} from "@edition/user/components/UserDataTableFilterInputComponent";
+import Link from "next/link";
+import {IconMinus, IconSortAscending, IconSortDescending} from "@tabler/icons-react";
 
 export const UsersPage: React.FC = () => {
 
@@ -23,7 +24,13 @@ export const UsersPage: React.FC = () => {
         notFound()
     }
 
-    return <div style={{background: "#070514", height: "100%", padding: "1rem", borderTopLeftRadius: "1rem", borderTopRightRadius: "1rem"}}>
+    return <div style={{
+        background: "#070514",
+        height: "100%",
+        padding: "1rem",
+        borderTopLeftRadius: "1rem",
+        borderTopRightRadius: "1rem"
+    }}>
         <Flex align={"center"} justify={"space-between"}>
             <Flex style={{gap: "0.35rem", flexDirection: "column"}}>
                 <Text size={"xl"} hierarchy={"primary"}>
@@ -33,15 +40,77 @@ export const UsersPage: React.FC = () => {
                     Manage users who have access to your instance. You can invite new users and remove existing ones.
                 </Text>
             </Flex>
-            <Flex align={"center"} style={{gap: "0.7rem"}}>
+            <ButtonGroup>
                 <Button color={"success"}>Invite</Button>
-            </Flex>
+                <Menu>
+                    <MenuTrigger asChild>
+                        <Button color={"secondary"} variant={"filled"}>Sort</Button>
+                    </MenuTrigger>
+                    <MenuPortal>
+                        <MenuContent>
+                            <MenuCheckboxItem
+                                checked={sort["username"] === undefined ? "indeterminate" : sort["username"] === "asc"}
+                                onSelect={event => {
+                                    if (sort["username"] === undefined)
+                                        setSort(prev => ({...prev, username: "asc"}))
+                                    else if (sort["username"] === "asc")
+                                        setSort(prev => ({...prev, username: "desc"}))
+                                    else
+                                        setSort(prev => ({...prev, username: undefined}))
+
+                                    event.preventDefault()
+                                    event.stopPropagation()
+                                }}>
+                                {sort["username"] === undefined ? <IconMinus size={13}/> : sort["name"] === "asc" ?
+                                    <IconSortDescending size={13}/> : <IconSortAscending size={13}/>}
+                                Username
+                            </MenuCheckboxItem>
+                            <MenuCheckboxItem
+                                checked={sort["createdAt"] === undefined ? "indeterminate" : sort["createdAt"] === "asc"}
+                                onSelect={event => {
+                                    if (sort["createdAt"] === undefined)
+                                        setSort(prev => ({...prev, createdAt: "asc"}))
+                                    else if (sort["createdAt"] === "asc")
+                                        setSort(prev => ({...prev, createdAt: "desc"}))
+                                    else
+                                        setSort(prev => ({...prev, createdAt: undefined}))
+
+                                    event.preventDefault()
+                                    event.stopPropagation()
+                                }}>
+                                {sort["createdAt"] === undefined ?
+                                    <IconMinus size={13}/> : sort["createdAt"] === "asc" ?
+                                        <IconSortDescending size={13}/> : <IconSortAscending size={13}/>}
+                                Created At
+                            </MenuCheckboxItem>
+                            <MenuCheckboxItem
+                                checked={sort["updatedAt"] === undefined ? "indeterminate" : sort["updatedAt"] === "asc"}
+                                onSelect={event => {
+                                    if (sort["updatedAt"] === undefined)
+                                        setSort(prev => ({...prev, updatedAt: "asc"}))
+                                    else if (sort["updatedAt"] === "asc")
+                                        setSort(prev => ({...prev, updatedAt: "desc"}))
+                                    else
+                                        setSort(prev => ({...prev, updatedAt: undefined}))
+
+                                    event.preventDefault()
+                                    event.stopPropagation()
+                                }}>
+                                {sort["updatedAt"] === undefined ?
+                                    <IconMinus size={13}/> : sort["updatedAt"] === "asc" ?
+                                        <IconSortDescending size={13}/> : <IconSortAscending size={13}/>}
+                                Updated At
+                            </MenuCheckboxItem>
+                        </MenuContent>
+                    </MenuPortal>
+                </Menu>
+            </ButtonGroup>
         </Flex>
         <Spacing spacing={"xl"}/>
         <div style={{width: "100%"}}>
             <UserDataTableFilterInputComponent onChange={filter => setFilter(filter)}/>
         </div>
         <Spacing spacing={"xl"}/>
-        <UserDataTableComponent filter={filter}/>
+        <UserDataTableComponent sort={sort} filter={filter}/>
     </div>
 }
