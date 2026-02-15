7c733aaef08ba42b3bead79d155cf892577e59b4
nicosammito <nico@sammito.de>
2026-02-15 22:27:49 +0100

    feat: implement projectDelete and projectUpdate mutations in ProjectService with corresponding GraphQL files

--- src/packages/ce/src/project/services/Project.service.ts
+++ src/packages/ce/src/project/services/Project.service.ts
@@ -13,11 +13,16 @@ import {
     NamespacesProjectsCreatePayload,
     NamespacesProjectsDeleteInput,
     NamespacesProjectsDeletePayload,
+    NamespacesProjectsUpdateInput,
+    NamespacesProjectsUpdatePayload,
     Query
 } from "@code0-tech/sagittarius-graphql-types";
 import {GraphqlClient} from "@core/util/graphql-client";
 import projectsQuery from "./queries/Projects.query.graphql"
 import projectCreateMutation from "./mutations/Project.create.mutation.graphql"
+import projectDeleteMutation from "./mutations/Project.delete.mutation.graphql"
+import projectUpdateMutation from "./mutations/Project.update.mutation.graphql"
+import projectAssignRuntimesMutation from "./mutations/Project.assignRuntimes.mutation.graphql"
 import {View} from "@code0-tech/pictor/dist/utils/view";
 
 export class ProjectService extends DNamespaceProjectReactiveService {
@@ -68,7 +73,28 @@ export class ProjectService extends DNamespaceProjectReactiveService {
     }
 
     async projectAssignRuntimes(payload: NamespacesProjectsAssignRuntimesInput): Promise<NamespacesProjectsAssignRuntimesPayload | undefined> {
-        return Promise.resolve(undefined);
+        const result = await this.client.mutate<Mutation, NamespacesProjectsAssignRuntimesInput>({
+            mutation: projectAssignRuntimesMutation,
+            variables: {
+                ...payload
+            }
+        })
+
+        if (result.data && result.data.namespacesProjectsAssignRuntimes && result.data.namespacesProjectsAssignRuntimes.namespaceProject) {
+            const project = result.data.namespacesProjectsAssignRuntimes.namespaceProject
+            const index = this.values({namespaceId: project?.namespace?.id}).findIndex(o => o.id === project.id)
+            const projectStored = this.values({namespaceId: project?.namespace?.id}).find(o => o.id === project.id)
+            this.set(index, new View(new DNamespaceProjectView({
+                ...projectStored?.json(),
+                runtimes: {
+                    count: payload.runtimeIds.length,
+                    nodes: payload.runtimeIds.map(runtimeId => ({ id: runtimeId }))
+                }
+            })))
+
+        }
+
+        return result.data?.namespacesProjectsAssignRuntimes ?? undefined
     }
 
     async projectCreate(payload: NamespacesProjectsCreateInput): Promise<NamespacesProjectsCreatePayload | undefined> {
@@ -90,7 +116,46 @@ export class ProjectService extends DNamespaceProjectReactiveService {
     }
 
     async projectDelete(payload: NamespacesProjectsDeleteInput): Promise<NamespacesProjectsDeletePayload | undefined> {
-        return Promise.resolve(undefined);
+        const result = await this.client.mutate<Mutation, NamespacesProjectsDeleteInput>({
+            mutation: projectDeleteMutation,
+            variables: {
+                ...payload
+            }
+        })
+
+        if (result.data && result.data.namespacesProjectsDelete && result.data.namespacesProjectsDelete.namespaceProject) {
+            const project = result.data.namespacesProjectsDelete.namespaceProject
+            const index = this.values({namespaceId: project?.namespace?.id}).findIndex(o => o.id === project.id)
+            this.delete(index)
+
+        }
+
+        return result.data?.namespacesProjectsDelete ?? undefined
+    }
+
+    async projectUpdate(payload: NamespacesProjectsUpdateInput): Promise<NamespacesProjectsUpdatePayload | undefined> {
+        const result = await this.client.mutate<Mutation, NamespacesProjectsUpdateInput>({
+            mutation: projectUpdateMutation,
+            variables: {
+                ...payload
+            }
+        })
+
+        if (result.data && result.data.namespacesProjectsUpdate && result.data.namespacesProjectsUpdate.namespaceProject) {
+            const project = result.data.namespacesProjectsUpdate.namespaceProject
+            const index = this.values({namespaceId: project?.namespace?.id}).findIndex(o => o.id === project.id)
+            const projectStored = this.values({namespaceId: project?.namespace?.id}).find(o => o.id === project.id)
+            this.set(index, new View(new DNamespaceProjectView({
+                ...projectStored?.json(),
+                ...(payload.name ? {name: payload.name} : {}),
+                ...(payload.description ? {description: payload.description} : {}),
+                ...(payload.slug ? {slug: payload.slug} : {}),
+                ...(payload.primaryRuntimeId ? {primaryRuntime: {id: payload.primaryRuntimeId}} : {}),
+            })))
+
+        }
+
+        return result.data?.namespacesProjectsUpdate ?? undefined
     }
 
 }
