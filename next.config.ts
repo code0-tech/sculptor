4cfbe3a6c7fd53872041f022828485b4484ccc59
nicosammito <nico@sammito.de>
2026-03-18 16:08:40 +0100

    feat: update Content Security Policy in next.config.ts for improved security

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,15 @@ const SAGITTARIUS_GRAPHQL_URL = process.env.SAGITTARIUS_GRAPHQL_URL ?? 'http://l
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' blob:;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
+    img-src 'self';
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    worker-src 'self' blob:;
+    worker-src 'self' blob: data: *;
 `
 
 const nextConfig: NextConfig = {
