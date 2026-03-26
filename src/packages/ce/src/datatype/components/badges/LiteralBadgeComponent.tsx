b9298a3ac2bd519751ea595820434364ac08c9c4
nicosammito <nsammito@code0.tech>
2026-03-26 23:05:09 +0100

    feat: enhance LiteralBadgeComponent to include tooltip for truncated values and improve text display

--- src/packages/ce/src/datatype/components/badges/LiteralBadgeComponent.tsx
+++ src/packages/ce/src/datatype/components/badges/LiteralBadgeComponent.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import {LiteralValue} from "@code0-tech/sagittarius-graphql-types";
-import {Badge, BadgeType, Text} from "@code0-tech/pictor";
+import {Badge, BadgeType, Text, Tooltip, TooltipContent, TooltipPortal, TooltipTrigger} from "@code0-tech/pictor";
+import {truncateText} from "@edition/flow/components/folder/FlowFolderComponent";
+import {DataTypeJSONInputTreeComponent} from "@edition/datatype/components/inputs/json/DataTypeJSONInputTreeComponent";
+import { EditableJSONEntry } from "../inputs/json/DataTypeJSONInputComponent";
 
 export interface LiteralBadgeComponentProps extends Omit<BadgeType, 'value' | 'children'> {
     value: LiteralValue
@@ -10,16 +13,45 @@ export const LiteralBadgeComponent: React.FC<LiteralBadgeComponentProps> = (prop
 
     const {value, ...rest} = props
 
+    const truncatedValue = React.useMemo(() => {
+        return truncateText(JSON.stringify(value.value), 75)
+    }, [value])
+
     return <Badge style={{verticalAlign: "middle"}}
                   color={"secondary"}
                   {...rest}>
-        <Text size={"sm"} style={{
-            maxWidth: "50px",
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-        }}>
-            {JSON.stringify(value.value)}
-        </Text>
+        {
+            !JSON.stringify(value).includes(truncatedValue) ? (
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <Text size={"sm"}>
+                            {truncatedValue}
+                        </Text>
+                    </TooltipTrigger>
+                    <TooltipPortal>
+                        <TooltipContent sideOffset={8} color={ Array.isArray(value.value) || typeof value.value === "object" ? "primary" : "secondary"}>
+                            {
+                                Array.isArray(value.value) || typeof value.value === "object" ? (
+                                    <DataTypeJSONInputTreeComponent object={value}
+                                                                    onEntryClick={function (entry: EditableJSONEntry): void {
+
+                                                                    }} collapsedState={{}}
+                                                                    setCollapsedState={function (path: string[], collapsed: boolean): void {
+                                                                    }}/>
+                                ) : (
+                                    <Text size={"sm"}>
+                                        {JSON.stringify(value.value)}
+                                    </Text>
+                                )
+                            }
+                        </TooltipContent>
+                    </TooltipPortal>
+                </Tooltip>
+            ) : (
+                <Text size={"sm"}>
+                    {truncatedValue}
+                </Text>
+            )
+        }
     </Badge>
 }
