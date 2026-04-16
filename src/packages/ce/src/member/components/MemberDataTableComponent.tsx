1cdc5c855a16adde4459daf8a91103bdc54a23bb
nicosammito <nsammito@code0.tech>
2026-04-16 21:27:52 +0200

    feat: simplify imports in MemberDataTableComponent by removing unused dependencies

--- src/packages/ce/src/member/components/MemberDataTableComponent.tsx
+++ src/packages/ce/src/member/components/MemberDataTableComponent.tsx
@@ -1,22 +1,7 @@
 import React from "react";
 import {DataTable, DataTableColumn, Text, useService, useStore} from "@code0-tech/pictor";
 import {DataTableFilterProps, DataTableSortProps} from "@code0-tech/pictor/dist/components/data-table/DataTable";
-import {
-    Namespace,
-    NamespaceMember,
-    NamespaceProject,
-    NamespaceRole,
-    Runtime,
-    User
-} from "@code0-tech/sagittarius-graphql-types";
-import {ProjectService} from "@edition/project/services/Project.service";
-import {ProjectDataTableRowComponent} from "@edition/project/components/ProjectDataTableRowComponent";
-import {RoleService} from "@edition/role/services/Role.service";
-import {RoleDataTableRowComponent} from "@edition/role/components/RoleDataTableRowComponent";
-import {RuntimeService} from "@edition/runtime/services/Runtime.service";
-import {RuntimeDataTableRowComponent} from "@edition/runtime/components/RuntimeDataTableRowComponent";
-import {UserDataTableRowComponent} from "@edition/user/components/UserDataTableRowComponent";
-import {UserService} from "@edition/user/services/User.service";
+import {Namespace, NamespaceMember} from "@code0-tech/sagittarius-graphql-types";
 import {MemberService} from "@edition/member/services/Member.service";
 import {MemberDataTableRowComponent} from "@edition/member/components/MemberDataTableRowComponent";
 
