fccdb00cd18bc56ac11fc29962ebce0eb8d097c7
nicosammito <nsammito@code0.tech>
2026-04-24 00:42:33 +0200

    feat: optimize useSelectedFunctionNode hook to memoize selected node

--- src/packages/ce/src/function/hooks/FunctionNode.selected.hook.ts
+++ src/packages/ce/src/function/hooks/FunctionNode.selected.hook.ts
@@ -1,8 +1,10 @@
 import {Node, useReactFlow, useStore} from "@xyflow/react";
 import {shallow} from "zustand/shallow";
+import React, {useState} from "react";
 
 export const useSelectedFunctionNode = () => {
-    return useStore((s) => s.nodes.filter((node) => node.selected)[0], shallow) as Node | undefined
+    const localSelectedNode = useStore((s) => s.nodes.filter((node) => node.selected)[0], shallow) as Node | undefined
+    return React.useMemo(() => localSelectedNode, [localSelectedNode?.id])
 }
 
 export const setSelectedFunctionNode = (nodeId: string, reactFlow: ReturnType<typeof useReactFlow>) => {
