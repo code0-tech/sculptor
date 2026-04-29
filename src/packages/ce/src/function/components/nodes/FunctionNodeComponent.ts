72589011d51135f97b257df9eebed94b34fa75de
nicosammito <nsammito@code0.tech>
2026-04-29 12:41:58 +0200

    feat: adding node schema to react flow node props

--- src/packages/ce/src/function/components/nodes/FunctionNodeComponent.ts
+++ src/packages/ce/src/function/components/nodes/FunctionNodeComponent.ts
@@ -1,9 +1,11 @@
 import {Flow, NodeFunction} from "@code0-tech/sagittarius-graphql-types";
 import {Component} from "@code0-tech/pictor";
+import {NodeSchema} from "@code0-tech/triangulum";
 
 export interface FunctionNodeComponentProps extends Record<string, unknown>, Component<HTMLDivElement> {
-    nodeId: NodeFunction['id']
+    nodeId?: NodeFunction['id']
     flowId: Flow['id']
+    schema: NodeSchema
     color: string
     parentNodeId?: NodeFunction['id']
     isParameter?: boolean
