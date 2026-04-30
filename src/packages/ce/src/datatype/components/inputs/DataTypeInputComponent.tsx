1885c846bde72b6b82c2203ac49a6b81f6662d22
nicosammito <nsammito@code0.tech>
2026-04-30 13:25:05 +0200

    feat: adding suggestions

--- src/packages/ce/src/datatype/components/inputs/DataTypeInputComponent.tsx
+++ src/packages/ce/src/datatype/components/inputs/DataTypeInputComponent.tsx
@@ -3,6 +3,13 @@ import {DataTypeTextInputComponent} from "./text/DataTypeTextInputComponent";
 import {InputProps} from "@code0-tech/pictor";
 import {DataTypeJSONInputComponent} from "@edition/datatype/components/inputs/json/DataTypeJSONInputComponent";
 import {NodeSchema} from "@code0-tech/triangulum";
+import {LiteralValue, NodeFunction, ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
+import {
+    FunctionSuggestion,
+    FunctionSuggestionType
+} from "@edition/function/components/suggestion/FunctionSuggestionComponent.view";
+import {toInputSuggestions} from "@edition/function/components/suggestion/FunctionSuggestionMenuComponent.util";
+import {FALLBACK_FUNCTION_NAME} from "@core/util/fallback-translations";
 
 export interface DataTypeInputComponentProps extends Omit<InputProps<any | null>, "wrapperComponent"> {
     schema: NodeSchema
@@ -14,15 +21,29 @@ export const DataTypeInputComponent: React.FC<DataTypeInputComponentProps> = (pr
 
     const {schema, ...rest} = props
 
+    const suggestions = schema?.schema?.suggestions as (NodeFunction | ReferenceValue | LiteralValue)[]
+    const functionSuggestions: FunctionSuggestion[] = suggestions?.map((suggest: (NodeFunction | ReferenceValue | LiteralValue)) => {
+
+        return {
+            type: suggest.__typename === "NodeFunction" ? FunctionSuggestionType.FUNCTION : suggest.__typename === "ReferenceValue" ? FunctionSuggestionType.REF_OBJECT : FunctionSuggestionType.VALUE,
+            value: suggest,
+            displayText: suggest.__typename === "NodeFunction" ? suggest.functionDefinition?.names?.[0]?.content ?? FALLBACK_FUNCTION_NAME : suggest?.value,
+            path: []
+        }
+
+    }) ?? []
+
     switch (schema?.schema?.input) {
         case "data":
         case "list":
             return <DataTypeJSONInputComponent
                 schema={schema}
+                suggestions={toInputSuggestions(functionSuggestions)}
                 {...rest}
             />
         default:
             return <DataTypeTextInputComponent
+                suggestions={toInputSuggestions(functionSuggestions)}
                 schema={schema}
                 {...rest}
             />
