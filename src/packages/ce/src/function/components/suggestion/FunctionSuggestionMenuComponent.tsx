0dec4789e4ee891c76972b956b3435d21dda554f
nicosammito <nsammito@code0.tech>
2026-05-03 15:54:23 +0200

    feat: refine suggestions type in FunctionSuggestionMenuComponent for improved consistency

--- src/packages/ce/src/function/components/suggestion/FunctionSuggestionMenuComponent.tsx
+++ src/packages/ce/src/function/components/suggestion/FunctionSuggestionMenuComponent.tsx
@@ -3,7 +3,7 @@ import {FunctionSuggestion} from "./FunctionSuggestionComponent.view";
 import {toInputSuggestions} from "./FunctionSuggestionMenuComponent.util";
 import {FunctionSuggestionSearchBarComponent} from "./FunctionSuggestionSearchBarComponent";
 import {
-    Card,
+    Card, InputSuggestion,
     InputSuggestionMenuContent,
     InputSuggestionMenuContentItems,
     InputSuggestionMenuContentItemsHandle,
@@ -14,7 +14,7 @@ import {
 
 export interface FunctionSuggestionMenuComponentProps {
     triggerContent: React.ReactNode
-    suggestions?: FunctionSuggestion[]
+    suggestions?: InputSuggestion[]
     onSuggestionSelect?: (suggestion: FunctionSuggestion) => void
 }
 
@@ -22,46 +22,17 @@ export const FunctionSuggestionMenuComponent: React.FC<FunctionSuggestionMenuCom
 
     const {suggestions = [], triggerContent, onSuggestionSelect = () => null} = props
 
-    const menuRef = React.useRef<InputSuggestionMenuContentItemsHandle | null>(null); // Ref to suggestion list
-    const [stateSuggestions, setStateSuggestions] = React.useState(suggestions)
-
-    React.useEffect(() => {
-        setStateSuggestions(suggestions)
-    }, [suggestions])
-
     return <Menu>
         <MenuTrigger asChild>
             {triggerContent}
         </MenuTrigger>
         <MenuPortal>
-            <InputSuggestionMenuContent align={"center"} color={"secondary"} data-qa-selector={"flow-builder-suggestions"}>
-                <FunctionSuggestionSearchBarComponent onType={event => {
-
-                    if (event.key === "ArrowDown") {
-                        event.preventDefault();
-                        menuRef.current?.focusFirstItem(); // Navigate down
-                    } else if (event.key === "ArrowUp") {
-                        event.preventDefault();
-                        menuRef.current?.focusLastItem(); // Navigate up
-                    }
-
-                    // @ts-ignore
-                    const searchTerm = event.target.value
-                    setStateSuggestions(suggestions.filter(suggestion => {
-                        return suggestion.displayText.some(text => {
-                            return text.includes(searchTerm)
-                        })
-                    }))
-                    event.preventDefault()
-                    return false
-                }}/>
+            <InputSuggestionMenuContent align={"end"} color={"secondary"} data-qa-selector={"flow-builder-suggestions"}>
                 <Card paddingSize={"xxs"} mt={-0.2} mx={-0.2}>
                     <InputSuggestionMenuContentItems
-                        /* @ts-ignore */
-                        ref={menuRef}
-                        suggestions={toInputSuggestions(stateSuggestions)}
+                        suggestions={suggestions}
                         onSuggestionSelect={(suggestion) => {
-                            onSuggestionSelect(suggestion.valueData as FunctionSuggestion)
+                            onSuggestionSelect(suggestion.value)
                         }}
                     />
                 </Card>
