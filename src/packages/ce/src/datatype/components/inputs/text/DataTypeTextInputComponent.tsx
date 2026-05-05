c6727890fef1168a3d2d7fbe797a30d04409f246
nicosammito <nsammito@code0.tech>
2026-05-05 16:19:42 +0200

    feat: enhance DataTypeTextInputComponent to improve value handling and validation

--- src/packages/ce/src/datatype/components/inputs/text/DataTypeTextInputComponent.tsx
+++ src/packages/ce/src/datatype/components/inputs/text/DataTypeTextInputComponent.tsx
@@ -7,50 +7,59 @@ import {StreamLanguage} from "@codemirror/language";
 import {tags} from "@lezer/highlight";
 import {ButtonGroup} from "@code0-tech/pictor/dist/components/button-group/ButtonGroup";
 import {IconVariable, IconX} from "@tabler/icons-react";
-import {useDebounce, useDebouncedCallback} from "use-debounce";
+import {useDebouncedCallback} from "use-debounce";
+import {LiteralValue, NodeFunction, NodeParameterValue} from "@code0-tech/sagittarius-graphql-types";
 
 export type DataTypeTextInputComponentProps = DataTypeInputComponentProps
 
 export const DataTypeTextInputComponent: React.FC<DataTypeTextInputComponentProps> = (props) => {
 
-    const {formValidation, title, initialValue, description, suggestions, onChange} = props
+    const {formValidation, title, initialValue, description, suggestions} = props
 
-    const defaultValue = React.useMemo(() => initialValue ?? "", [])
-    const onChangeDebounced = useDebouncedCallback(() => {
-        onChange?.({} as any)
+    const defaultValue: NodeParameterValue | NodeFunction | undefined = React.useMemo(() => initialValue ?? undefined, [])
+    const onChangeDebounced = useDebouncedCallback((value: string) => {
+        formValidation?.setValue?.(value ? {__typename: "LiteralValue", value: value} : undefined)
     }, 200)
 
     return React.useMemo(() => <>
         <InputLabel>{title}</InputLabel>
         <InputDescription>{description}</InputDescription>
-        {initialValue?.__typename === "NodeFunction" || initialValue?.__typename === "NodeFunctionIdWrapper" ? (
-            <NodeBadgeComponent value={initialValue}/>
-        ) : initialValue?.__typename === "ReferenceValue" ? (
-            <ReferenceBadgeComponent value={initialValue}/>
+        {defaultValue?.__typename === "NodeFunction" || defaultValue?.__typename === "NodeFunctionIdWrapper" ? (
+            <NodeBadgeComponent value={defaultValue}/>
+        ) : defaultValue?.__typename === "ReferenceValue" ? (
+            <ReferenceBadgeComponent value={defaultValue}/>
         ) : (
-            <EditorInput value={String(defaultValue)} onChange={onChangeDebounced} formValidation={formValidation} maw={"100%"} placeholder={"sd"} language={StreamLanguage.define({
-                token(stream) {
-                    stream.next()
-                    return null;
-                }
-            })} tokenStyles={[
-                {tag: tags.keyword, color: hashToColor("bracket")},
-            ]} right={
-                (suggestions?.length ?? 0) > 0 ? (
-                    <ButtonGroup color={"primary"}>
-                        <Button paddingSize={"xxs"}>
-                            <IconVariable size={13}/>
-                        </Button>
-                        <Button paddingSize={"xxs"}>
-                            <IconX size={13}/>
-                        </Button>
-                    </ButtonGroup>
-                ) : (
-                    <Button color={"primary"} paddingSize={"xxs"}>
-                        <IconX size={13}/>
-                    </Button>
-                )
-            } rightType={"action"}/>
+            <EditorInput value={(defaultValue as LiteralValue)?.value}
+                         onChange={onChangeDebounced}
+                         formValidation={{...formValidation, setValue: undefined}}
+                         maw={"100%"}
+                         placeholder={String(title) ?? ""}
+                         language={StreamLanguage.define({
+                             token(stream) {
+                                 stream.next()
+                                 return null;
+                             }
+                         })}
+                         tokenStyles={[
+                             {tag: tags.keyword, color: hashToColor("bracket")},
+                         ]}
+                         right={
+                             (suggestions?.length ?? 0) > 0 ? (
+                                 <ButtonGroup color={"primary"}>
+                                     <Button paddingSize={"xxs"}>
+                                         <IconVariable size={13}/>
+                                     </Button>
+                                     <Button paddingSize={"xxs"}>
+                                         <IconX size={13}/>
+                                     </Button>
+                                 </ButtonGroup>
+                             ) : (
+                                 <Button color={"primary"} paddingSize={"xxs"}>
+                                     <IconX size={13}/>
+                                 </Button>
+                             )
+                         }
+                         rightType={"action"}/>
         )}
     </>, [formValidation])
 }
