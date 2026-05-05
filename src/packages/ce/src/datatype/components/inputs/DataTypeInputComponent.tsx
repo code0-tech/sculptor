bc1770235e7e36b9fbb5bfd3ffe5167571dc6fc0
nicosammito <nsammito@code0.tech>
2026-05-05 23:33:27 +0200

    feat: update DataTypeInputComponent to enhance props and simplify input handling

--- src/packages/ce/src/datatype/components/inputs/DataTypeInputComponent.tsx
+++ src/packages/ce/src/datatype/components/inputs/DataTypeInputComponent.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import {DataTypeTextInputComponent} from "./text/DataTypeTextInputComponent";
-import {DataTypeJSONInputComponent} from "@edition/datatype/components/inputs/json/DataTypeJSONInputComponent";
 import {NodeSchema} from "@code0-tech/triangulum";
 import {LiteralValue, NodeFunction, NodeParameterValue, ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
-import {
-    FunctionSuggestion,
-    FunctionSuggestionType
-} from "@edition/function/components/suggestion/FunctionSuggestionComponent.view";
-import {toInputSuggestions} from "@edition/function/components/suggestion/FunctionSuggestionMenuComponent.util";
-import {FALLBACK_FUNCTION_NAME} from "@core/util/fallback-translations";
 import {DataTypeBooleanInputComponent} from "@edition/datatype/components/inputs/boolean/DataTypeBooleanInputComponent";
 import {DataTypeSelectInputComponent} from "@edition/datatype/components/inputs/select/DataTypeSelectInputComponent";
 import {InputWrapperProps} from "@code0-tech/pictor/dist/components/form/InputWrapper";
 
-export interface DataTypeInputComponentProps extends Omit<InputWrapperProps<NodeParameterValue | NodeFunction>, "wrapperComponent"> {
+export interface DataTypeInputComponentProps extends Omit<InputWrapperProps<NodeParameterValue | NodeFunction>, "wrapperComponent" | "onChange"> {
     schema: NodeSchema
     clearable?: boolean
+    onChange?: (value: NodeParameterValue | NodeFunction | undefined) => void
     suggestions?: (NodeFunction | ReferenceValue | LiteralValue)[]
     onClear?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
@@ -27,13 +21,6 @@ export const DataTypeInputComponent: React.FC<DataTypeInputComponentProps> = (pr
     const suggestions = schema?.schema?.suggestions as (NodeFunction | ReferenceValue | LiteralValue)[]
 
     switch (schema?.schema?.input) {
-        case "data":
-        case "list":
-            return <DataTypeJSONInputComponent
-                schema={schema}
-                suggestions={suggestions}
-                {...rest}
-            />
         case "boolean":
             return <DataTypeBooleanInputComponent
                 schema={schema}
