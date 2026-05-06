52a20091004a7588bf78dd23434581a29454b7c4
nicosammito <nsammito@code0.tech>
2026-05-06 19:07:29 +0200

    feat: enhance DataTypeTextInputComponent to support additional value types and integrate input controls

--- src/packages/ce/src/datatype/components/inputs/text/DataTypeTextInputComponent.tsx
+++ src/packages/ce/src/datatype/components/inputs/text/DataTypeTextInputComponent.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import {DataTypeInputComponentProps} from "../DataTypeInputComponent";
-import {
-    Button,
-    EditorInput,
-    Flex,
-    hashToColor,
-    InputDescription,
-    InputLabel,
-    Menu,
-    MenuContent,
-    MenuItem,
-    MenuPortal,
-    MenuTrigger
-} from "@code0-tech/pictor";
+import {EditorInput, hashToColor, InputDescription, InputLabel} from "@code0-tech/pictor";
 import {NodeBadgeComponent} from "@edition/datatype/components/badges/NodeBadgeComponent";
 import {ReferenceBadgeComponent} from "@edition/datatype/components/badges/ReferenceBadgeComponent";
 import {StreamLanguage} from "@codemirror/language";
 import {tags} from "@lezer/highlight";
-import {ButtonGroup} from "@code0-tech/pictor/dist/components/button-group/ButtonGroup";
-import {IconVariable, IconX} from "@tabler/icons-react";
 import {useDebouncedCallback} from "use-debounce";
-import {LiteralValue, NodeFunction, NodeParameterValue} from "@code0-tech/sagittarius-graphql-types";
+import {LiteralValue, NodeFunction, NodeParameterValue, ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
+import {DataTypeInputControlsComponent} from "@edition/datatype/components/inputs/DataTypeInputControlsComponent";
 
 export type DataTypeTextInputComponentProps = DataTypeInputComponentProps
 
@@ -29,9 +16,15 @@ export const DataTypeTextInputComponent: React.FC<DataTypeTextInputComponentProp
     const {formValidation, title, initialValue, description, suggestions, onChange} = props
 
     const defaultValue: NodeParameterValue | NodeFunction | undefined = React.useMemo(() => initialValue ?? undefined, [initialValue])
-    const onChangeDebounced = useDebouncedCallback((value: string | undefined) => {
-        formValidation?.setValue?.(value ? {__typename: "LiteralValue", value: value} : undefined)
-        onChange?.(value ? {__typename: "LiteralValue", value: value} : undefined)
+    const onChangeDebounced = useDebouncedCallback((value: string | LiteralValue | NodeFunction | ReferenceValue | undefined) => {
+
+        if (typeof value === "string") {
+            formValidation?.setValue?.(value ? {__typename: "LiteralValue", value: value} : undefined)
+            onChange?.(value ? {__typename: "LiteralValue", value: value} : undefined)
+        } else {
+            formValidation?.setValue?.(value)
+            onChange?.(value)
+        }
     }, 200)
 
     return React.useMemo(() => <>
@@ -57,47 +50,7 @@ export const DataTypeTextInputComponent: React.FC<DataTypeTextInputComponentProp
                              {tag: tags.keyword, color: hashToColor("bracket")},
                          ]}
                          right={
-                             (suggestions?.length ?? 0) > 0 ? (
-                                 <ButtonGroup color={"primary"}>
-                                     <Menu>
-                                         <MenuTrigger asChild>
-                                             <Button paddingSize={"xxs"}>
-                                                 <IconVariable size={13}/>
-                                             </Button>
-                                         </MenuTrigger>
-                                         <MenuPortal>
-                                             <MenuContent>
-                                                 {suggestions?.map((suggest, index) => {
-                                                     if (suggest.__typename === "LiteralValue") {
-                                                         return <MenuItem>
-                                                             <Flex style={{gap: "0.35rem"}} align={"center"}>
-                                                                 {(suggest)?.value.toString()}
-                                                             </Flex>
-                                                         </MenuItem>
-                                                     }
-
-                                                     if (suggest.__typename === "ReferenceValue") {
-                                                         return <MenuItem>
-                                                             <ReferenceBadgeComponent value={suggest}/>
-                                                         </MenuItem>
-                                                     }
-                                                 })}
-                                             </MenuContent>
-                                         </MenuPortal>
-                                     </Menu>
-                                     <Button paddingSize={"xxs"} onClick={() => {
-                                         onChangeDebounced(undefined)
-                                     }}>
-                                         <IconX size={13}/>
-                                     </Button>
-                                 </ButtonGroup>
-                             ) : (
-                                 <Button color={"primary"} paddingSize={"xxs"} onClick={() => {
-                                     onChangeDebounced(undefined)
-                                 }}>
-                                     <IconX size={13}/>
-                                 </Button>
-                             )
+                             <DataTypeInputControlsComponent suggestions={suggestions} onSelect={onChangeDebounced}/>
                          }
                          rightType={"action"}/>
         )}
