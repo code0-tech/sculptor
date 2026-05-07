80dcc9e14312908c9a91a4e2b3cc843737482548
nicosammito <nsammito@code0.tech>
2026-05-07 15:10:27 +0200

    feat: refactor DataTypeBooleanInputComponent to use DataTypeInputValueComponent for improved value handling

--- src/packages/ce/src/datatype/components/inputs/boolean/DataTypeBooleanInputComponent.tsx
+++ src/packages/ce/src/datatype/components/inputs/boolean/DataTypeBooleanInputComponent.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import {DataTypeInputComponentProps} from "@edition/datatype/components/inputs/DataTypeInputComponent";
 import {InputDescription, InputLabel, SegmentedControl, SegmentedControlItem} from "@code0-tech/pictor";
-import {NodeBadgeComponent} from "@edition/datatype/components/badges/NodeBadgeComponent";
-import {ReferenceBadgeComponent} from "@edition/datatype/components/badges/ReferenceBadgeComponent";
-import {InputWrapper} from "@code0-tech/pictor/dist/components/form/InputWrapper";
 import {useDebouncedCallback} from "use-debounce";
 import {LiteralValue, NodeFunction, ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
-import {DataTypeInputControlsComponent} from "@edition/datatype/components/inputs/DataTypeInputControlsComponent";
+import {DataTypeInputValueComponent} from "@edition/datatype/components/inputs/DataTypeInputValueComponent";
 
 export type DataTypeBooleanInputComponentProps = DataTypeInputComponentProps
 
@@ -34,31 +31,23 @@ export const DataTypeBooleanInputComponent: React.FC<DataTypeBooleanInputCompone
     return React.useMemo(() => <>
         <InputLabel>{title}</InputLabel>
         <InputDescription>{description}</InputDescription>
-        <InputWrapper formValidation={{...formValidation, setValue: undefined}} right={
-            <DataTypeInputControlsComponent suggestions={suggestions} onSelect={onChangeDebounced}/>
-        } rightType={"action"}>
-            {initialValue?.__typename === "NodeFunction" || initialValue?.__typename === "NodeFunctionIdWrapper" ? (
-                <NodeBadgeComponent value={initialValue}/>
-            ) : initialValue?.__typename === "ReferenceValue" ? (
-                <ReferenceBadgeComponent value={initialValue}/>
-            ) : (
-                <div style={{alignSelf: "stretch", flex: "1 1 auto", padding: "0.175rem 0 0.175rem 0.175rem"}}>
-                    <SegmentedControl type={"single"}
-                                      h={"100%"}
-                                      bg={"transparent"}
-                                      style={{boxShadow: "none"}}
-                                      value={(initialValue as LiteralValue)?.value?.toString() ?? ""}
-                                      onValueChange={onChangeDebounced}>
-                        <SegmentedControlItem w={"100%"} value={"true"}>
-                            True
-                        </SegmentedControlItem>
-                        <SegmentedControlItem w={"100%"} value={"false"}>
-                            False
-                        </SegmentedControlItem>
-                    </SegmentedControl>
-                </div>
-            )}
-        </InputWrapper>
+        <DataTypeInputValueComponent inside initialValue={initialValue} onChange={onChangeDebounced}
+                                     suggestions={suggestions} formValidation={formValidation}>
+            <SegmentedControl type={"single"}
+                              h={"100%"}
+                              ml={-0.35}
+                              bg={"transparent"}
+                              style={{boxShadow: "none"}}
+                              value={(initialValue as LiteralValue)?.value?.toString() ?? ""}
+                              onValueChange={onChangeDebounced}>
+                <SegmentedControlItem w={"100%"} value={"true"}>
+                    True
+                </SegmentedControlItem>
+                <SegmentedControlItem w={"100%"} value={"false"}>
+                    False
+                </SegmentedControlItem>
+            </SegmentedControl>
+        </DataTypeInputValueComponent>
     </>, [formValidation, initialValue])
 
 }
