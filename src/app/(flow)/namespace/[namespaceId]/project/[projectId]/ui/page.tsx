5eff0f48ed51eded98327df030ab8e54904480cf
nicosammito <nsammito@code0.tech>
2026-05-14 19:01:23 +0200

    feat: create page.tsx to integrate UIEditorPage with necessary styles

---
+++ src/app/(flow)/namespace/[namespaceId]/project/[projectId]/ui/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+import "@puckeditor/core/puck.css";
+import '@xyflow/react/dist/style.css';
+import {UIEditorPage} from "@edition/ui/pages/UIEditorPage";
+
+
+export default UIEditorPage
