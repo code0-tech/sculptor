12eca2dec91826a1c8a994081ba255f422ee6cab
nicosammito <nsammito@code0.tech>
2026-05-24 01:04:36 +0200

    feat: add AuthLayout component for authentication page layout

---
+++ src/app/(flow)/namespace/[namespaceId]/project/[projectId]/module/[moduleId]/layout.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import React from "react";
+import {Col, Flex} from "@code0-tech/pictor";
+
+export default function AuthLayout({children}: Readonly<{ children: React.ReactNode }>) {
+    return <div style={{
+        background: "#070514",
+        height: "100%",
+        padding: "1rem",
+        borderTopLeftRadius: "1rem",
+        borderTopRightRadius: "1rem"
+    }}>
+        <Flex h={"100%"} w={"100%"} align={"center"} justify={"center"}>
+            <Col xs={4} style={{marginTop: "auto", marginBottom: "auto"}}>
+                {children}
+            </Col>
+        </Flex>
+    </div>
+}
