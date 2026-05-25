b87ac4ee31f81e5d8ea5b93f3b6dde6faeff4d6a
nicosammito <nsammito@code0.tech>
2026-05-25 13:16:47 +0200

    feat: add ApplicationRestrictionsView and ApplicationTabListView components for managing application settings

---
+++ src/packages/ce/src/application/views/ApplicationTabListView.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {TabList, TabTrigger} from "@code0-tech/pictor/dist/components/tab/Tab";
+import {Button, Text} from "@code0-tech/pictor";
+
+export const ApplicationTabListView: React.FC = () => {
+    return <TabList>
+        <TabTrigger value={"general"} w={"100%"} asChild>
+            <Button paddingSize={"xxs"} variant={"none"} justify={"start"}>
+                <Text size={"md"}>General</Text>
+            </Button>
+        </TabTrigger>
+        <TabTrigger value={"restrictions"} w={"100%"} asChild>
+            <Button paddingSize={"xxs"} variant={"none"} justify={"start"}>
+                <Text size={"md"}>Restrictions</Text>
+            </Button>
+        </TabTrigger>
+    </TabList>
+}
