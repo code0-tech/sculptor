a5116c5ca6a0733697056d8a6f5da8c88f42bee1
nicosammito <nsammito@code0.tech>
2026-05-31 18:23:02 +0200

    feat: enhance DataTypeSubFlowInputComponent to support SubFlowValue and integrate FlowService

--- src/packages/ce/src/datatype/components/inputs/sub-flow/DataTypeSubFlowInputComponent.tsx
+++ src/packages/ce/src/datatype/components/inputs/sub-flow/DataTypeSubFlowInputComponent.tsx
@@ -1,12 +1,14 @@
 import {DataTypeInputComponentProps} from "@edition/datatype/components/inputs/DataTypeInputComponent";
 import React from "react";
 import {useDebouncedCallback} from "use-debounce";
-import {InputDescription, InputLabel, Text} from "@code0-tech/pictor";
+import {InputDescription, InputLabel, Text, useService} from "@code0-tech/pictor";
 import lodash from "lodash"
 import {useFunctionSuggestions} from "@edition/function/hooks/Function.suggestion.hook";
 import {DataTypeInputValueComponent} from "@edition/datatype/components/inputs/DataTypeInputValueComponent";
 import {SuggestionDialogComponent} from "@edition/function/components/suggestion/SuggestionDialogComponent";
-import {LiteralValue, NodeFunction, ReferenceValue} from "@code0-tech/sagittarius-graphql-types";
+import {Flow, LiteralValue, NodeFunction, ReferenceValue, SubFlowValue} from "@code0-tech/sagittarius-graphql-types";
+import {FlowService} from "@edition/flow/services/Flow.service";
+import {useParams} from "next/navigation";
 
 
 export type DataTypeSubFlowInputComponentProps = DataTypeInputComponentProps
@@ -16,28 +18,45 @@ export const DataTypeSubFlowInputComponent: React.FC<DataTypeSubFlowInputCompone
 
     const {formValidation, title, initialValue, description, suggestions, onChange} = props
 
+    const params = useParams()
+    const flowService = useService(FlowService)
+
+    const flowIndex = params.flowId as any as number
+    const flowId: Flow['id'] = `gid://sagittarius/Flow/${flowIndex}`
+
     const defaultValue: number = React.useMemo(() => suggestions?.findIndex(suggest => {
         return initialValue && lodash.isMatch(initialValue, suggest)
     }), [suggestions])!
 
+
     const [suggestionDialogOpen, setSuggestionDialogOpen] = React.useState(false)
     const result = useFunctionSuggestions()
 
-    const onChangeDebounced = useDebouncedCallback((value: LiteralValue | ReferenceValue | NodeFunction | undefined) => {
+    const onChangeDebounced = useDebouncedCallback((value: LiteralValue | SubFlowValue | ReferenceValue | NodeFunction | undefined) => {
+
+        if (value?.__typename === "NodeFunction") {
+            const nodeId = flowService.addNodeById(flowId, value)
+            value = {
+                __typename: "SubFlowValue",
+                startingNodeId: nodeId
+            }
+        }
+
         formValidation?.setValue?.(value ?? undefined)
         onChange?.(value ?? undefined)
     }, 200)
 
     return React.useMemo(() => <>
-        <SuggestionDialogComponent suggestions={result}
+        <SuggestionDialogComponent suggestions={[...suggestions!, ...result]}
                                    open={suggestionDialogOpen}
                                    onSuggestionSelect={suggestion => {
-                                       onChangeDebounced(suggestion.value as NodeFunction)
+                                       onChangeDebounced(suggestion as NodeFunction)
                                    }}
                                    onOpenChange={setSuggestionDialogOpen}/>
         <InputLabel>{title}</InputLabel>
         <InputDescription>{description}</InputDescription>
         <DataTypeInputValueComponent inside
+                                     showSuggestions={false}
                                      wrapperComponent={{
                                          onClick: () => {
                                              setSuggestionDialogOpen(true)
