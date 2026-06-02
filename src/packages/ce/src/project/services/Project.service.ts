156be972697b7d1a858ad7a384c1f77d16d5113e
nicosammito <nsammito@code0.tech>
2026-06-02 14:10:20 +0200

    feat: implement project module configuration update functionality

--- src/packages/ce/src/project/services/Project.service.ts
+++ src/packages/ce/src/project/services/Project.service.ts
@@ -1,9 +1,7 @@
+import {ReactiveArrayService, ReactiveArrayStore} from "@code0-tech/pictor";
 import {
-    ReactiveArrayService,
-    ReactiveArrayStore
-} from "@code0-tech/pictor";
-import {
-    Mutation, Namespace,
+    Mutation,
+    Namespace,
     NamespaceProject,
     NamespacesProjectsAssignRuntimesInput,
     NamespacesProjectsAssignRuntimesPayload,
@@ -11,34 +9,37 @@ import {
     NamespacesProjectsCreatePayload,
     NamespacesProjectsDeleteInput,
     NamespacesProjectsDeletePayload,
+    NamespacesProjectsRuntimeAssignmentsUpdateModuleConfigurationsInput,
+    NamespacesProjectsRuntimeAssignmentsUpdateModuleConfigurationsPayload,
     NamespacesProjectsUpdateInput,
     NamespacesProjectsUpdatePayload,
     Query
 } from "@code0-tech/sagittarius-graphql-types";
 import {GraphqlClient} from "@core/util/graphql-client";
-import projectsQuery from "./queries/Projects.query.graphql"
-import projectCreateMutation from "./mutations/Project.create.mutation.graphql"
-import projectDeleteMutation from "./mutations/Project.delete.mutation.graphql"
-import projectUpdateMutation from "./mutations/Project.update.mutation.graphql"
-import projectAssignRuntimesMutation from "./mutations/Project.assignRuntimes.mutation.graphql"
+import projectsQuery from "@edition/project/services/queries/Projects.query.graphql"
+import projectCreateMutation from "@edition/project/services/mutations/Project.create.mutation.graphql"
+import projectDeleteMutation from "@edition/project/services/mutations/Project.delete.mutation.graphql"
+import projectUpdateMutation from "@edition/project/services/mutations/Project.update.mutation.graphql"
+import projectAssignRuntimesMutation from "@edition/project/services/mutations/Project.assignRuntimes.mutation.graphql"
+import projectUpdateModuleConfigurationMutation
+    from "@edition/project/services/mutations/Project.updateModuleConfiguration.graphql"
 import {View} from "@code0-tech/pictor/dist/utils/view";
-import {ProjectView} from "@edition/project/services/Project.view";
 
 export type ProjectDependencies = {
     namespaceId: Namespace['id']
 }
 
-export class ProjectService extends ReactiveArrayService<ProjectView, ProjectDependencies> {
+export class ProjectService extends ReactiveArrayService<NamespaceProject, ProjectDependencies> {
 
     private readonly client: GraphqlClient
     private i = 0;
 
-    constructor(client: GraphqlClient, store: ReactiveArrayStore<View<ProjectView>>) {
+    constructor(client: GraphqlClient, store: ReactiveArrayStore<View<NamespaceProject>>) {
         super(store);
         this.client = client
     }
 
-    values(dependencies: ProjectDependencies): ProjectView[] {
+    values(dependencies: ProjectDependencies): NamespaceProject[] {
         const projects = super.values()
         if (!dependencies?.namespaceId) return projects
 
@@ -61,7 +62,7 @@ export class ProjectService extends ReactiveArrayService<ProjectView, ProjectDep
                         project.namespace?.id === namespaceId &&
                         !this.hasById(project.id)
                     ) {
-                        this.set(this.i++, new View(new ProjectView(project)))
+                        this.set(this.i++, new View(project))
                     }
                 })
             })
@@ -75,7 +76,7 @@ export class ProjectService extends ReactiveArrayService<ProjectView, ProjectDep
         return project !== undefined
     }
 
-    getById(id: NamespaceProject['id'], dependencies?: ProjectDependencies): ProjectView | undefined {
+    getById(id: NamespaceProject['id'], dependencies?: ProjectDependencies): NamespaceProject | undefined {
         return this.values(dependencies!).find(project => project && project.id === id)
     }
 
@@ -91,14 +92,14 @@ export class ProjectService extends ReactiveArrayService<ProjectView, ProjectDep
             const project = result.data.namespacesProjectsAssignRuntimes.namespaceProject
             const index = this.values({namespaceId: project?.namespace?.id}).findIndex(o => o.id === project.id)
             const projectStored = this.values({namespaceId: project?.namespace?.id}).find(o => o.id === project.id)
-            this.set(index, new View(new ProjectView({
-                ...projectStored?.json(),
+            this.set(index, new View({
+                ...projectStored,
                 runtimes: {
                     count: payload.runtimeIds.length,
-                    nodes: payload.runtimeIds.map(runtimeId => ({ id: runtimeId }))
+                    nodes: payload.runtimeIds.map(runtimeId => ({id: runtimeId}))
                 },
                 primaryRuntime: result.data.namespacesProjectsAssignRuntimes.namespaceProject.primaryRuntime
-            })))
+            }))
 
         }
 
@@ -116,7 +117,7 @@ export class ProjectService extends ReactiveArrayService<ProjectView, ProjectDep
         if (result.data && result.data.namespacesProjectsCreate && result.data.namespacesProjectsCreate.namespaceProject) {
             const project = result.data.namespacesProjectsCreate.namespaceProject
             if (!this.hasById(project.id)) {
-                this.add(new View(new ProjectView(project)))
+                this.add(new View(project))
             }
         }
 
@@ -153,17 +154,46 @@ export class ProjectService extends ReactiveArrayService<ProjectView, ProjectDep
             const project = result.data.namespacesProjectsUpdate.namespaceProject
             const index = this.values({namespaceId: project?.namespace?.id}).findIndex(o => o.id === project.id)
             const projectStored = this.values({namespaceId: project?.namespace?.id}).find(o => o.id === project.id)
-            this.set(index, new View(new ProjectView({
-                ...projectStored?.json(),
+            this.set(index, new View({
+                ...projectStored,
                 ...(payload.name ? {name: payload.name} : {}),
                 ...(payload.description ? {description: payload.description} : {}),
                 ...(payload.slug ? {slug: payload.slug} : {}),
                 ...(payload.primaryRuntimeId ? {primaryRuntime: {id: payload.primaryRuntimeId}} : {}),
-            })))
+            }))
 
         }
 
         return result.data?.namespacesProjectsUpdate ?? undefined
+    }
+
+    async projectModuleConfigurationUpdate(payload: NamespacesProjectsRuntimeAssignmentsUpdateModuleConfigurationsInput): Promise<NamespacesProjectsRuntimeAssignmentsUpdateModuleConfigurationsPayload | undefined> {
+
+        const result = await this.client.mutate<Mutation, NamespacesProjectsRuntimeAssignmentsUpdateModuleConfigurationsInput>({
+            mutation: projectUpdateModuleConfigurationMutation,
+            variables: {
+                ...payload
+            }
+        })
+
+        if (result.data && result.data.namespacesProjectsRuntimeAssignmentsUpdateModuleConfigurations && result.data.namespacesProjectsRuntimeAssignmentsUpdateModuleConfigurations.namespaceProjectRuntimeAssignment) {
+            const project = super.values().find(p => p.runtimeAssignments?.nodes?.map(rA => rA?.id).includes(payload.namespaceProjectRuntimeAssignmentId))
+            const index = super.values().findIndex(p => p.runtimeAssignments?.nodes?.map(rA => rA?.id).includes(payload.namespaceProjectRuntimeAssignmentId))
+
+            this.set(index, new View({
+                ...project,
+                runtimeAssignments: {
+                    ...project?.runtimeAssignments,
+                    nodes: [
+                        ...(project?.runtimeAssignments?.nodes?.filter(rA => rA?.id !== payload.namespaceProjectRuntimeAssignmentId) ?? []),
+                        (result?.data.namespacesProjectsRuntimeAssignmentsUpdateModuleConfigurations.namespaceProjectRuntimeAssignment ?? {})
+                    ]
+                }
+            }))
+        }
+
+        return result?.data?.namespacesProjectsRuntimeAssignmentsUpdateModuleConfigurations ?? undefined
+
     }
 
 }
