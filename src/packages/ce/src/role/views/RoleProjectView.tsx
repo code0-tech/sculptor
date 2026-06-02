7456c9952ec12442e40344fa12baa5fae27e80f8
nicosammito <nsammito@code0.tech>
2026-06-02 14:10:30 +0200

    feat: update project components to use NamespaceProject for improved type handling

--- src/packages/ce/src/role/views/RoleProjectView.tsx
+++ src/packages/ce/src/role/views/RoleProjectView.tsx
@@ -8,7 +8,6 @@ import {RoleService} from "@edition/role/services/Role.service";
 import type {Namespace, NamespaceProject, NamespaceRole, Scalars} from "@code0-tech/sagittarius-graphql-types";
 import {ProjectDataTableComponent} from "@edition/project/components/ProjectDataTableComponent";
 import {ProjectMenuComponent} from "@edition/project/components/ProjectMenuComponent";
-import {ProjectView} from "@edition/project/services/Project.view";
 import {IconTrash} from "@tabler/icons-react";
 
 export const RoleProjectView: React.FC = () => {
@@ -60,7 +59,7 @@ export const RoleProjectView: React.FC = () => {
         })
     }
 
-    const filterNotAssignedProjects = React.useCallback((project: ProjectView) => {
+    const filterNotAssignedProjects = React.useCallback((project: NamespaceProject) => {
         return !assignedProjectIds.find(projectId => projectId == project.id!!)
     }, [assignedProjectIds])
 
