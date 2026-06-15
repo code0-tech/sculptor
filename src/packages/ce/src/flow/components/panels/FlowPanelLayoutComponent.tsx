97f7d3b29c96fadda3d373f001c5b0deaa43ad84
nicosammito <nsammito@code0.tech>
2026-06-15 14:43:41 +0200

    feat: removing layout control, because currently we only support 1 layout and replace it with flow name for now

--- src/packages/ce/src/flow/components/panels/FlowPanelLayoutComponent.tsx
+++ src/packages/ce/src/flow/components/panels/FlowPanelLayoutComponent.tsx
@@ -1,25 +1,35 @@
 import React from "react";
-import {IconLayout, IconLayoutDistributeHorizontal, IconLayoutDistributeVertical} from "@tabler/icons-react";
 import {Panel} from "@xyflow/react";
-import {
-    SegmentedControl,
-    SegmentedControlItem, Text,
-    Tooltip,
-    TooltipContent,
-    TooltipPortal,
-    TooltipTrigger
-} from "@code0-tech/pictor";
+import {Text, useService, useStore} from "@code0-tech/pictor";
+import {useParams} from "next/navigation";
+import {Flow, Namespace, NamespaceProject} from "@code0-tech/sagittarius-graphql-types";
+import {FlowService} from "@edition/flow/services/Flow.service";
 
-export interface FlowPanelLayoutComponentProps {
 
-}
+export const FlowPanelLayoutComponent: React.FC = () => {
 
-export const FlowPanelLayoutComponent: React.FC<FlowPanelLayoutComponentProps> = (props) => {
+    const params = useParams()
+    const flowService = useService(FlowService)
+    const flowStore = useStore(FlowService)
 
-    const {} = props
+    const namespaceIndex = params.namespaceId as any as number
+    const projectIndex = params.projectId as any as number
+    const flowIndex = params.flowId as any as number
+    const namespaceId: Namespace['id'] = `gid://sagittarius/Namespace/${namespaceIndex}`
+    const projectId: NamespaceProject['id'] = `gid://sagittarius/NamespaceProject/${projectIndex}`
+    const flowId: Flow['id'] = `gid://sagittarius/Flow/${flowIndex}`
+
+    const flow = React.useMemo(
+        () => flowService.getById(flowId, {
+            namespaceId,
+            projectId
+        }),
+        [flowId, flowStore, namespaceId, projectId]
+    )
 
     return <Panel position={"top-center"}>
-        <SegmentedControl type={"single"} defaultValue={"horizontal"}>
+        <Text style={{textAlign: "center"}}>{flow?.name}</Text>
+        {/*<SegmentedControl type={"single"} defaultValue={"horizontal"}>
             <Tooltip>
                 <TooltipTrigger asChild>
                     <SegmentedControlItem value={"horizontal"} display={"flex"}>
@@ -56,6 +66,6 @@ export const FlowPanelLayoutComponent: React.FC<FlowPanelLayoutComponentProps> =
                     </TooltipContent>
                 </TooltipPortal>
             </Tooltip>
-        </SegmentedControl>
+        </SegmentedControl>*/}
     </Panel>
 }
