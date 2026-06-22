4612581b2cda54532485077641be5b5fb84c35de
nicosammito <nsammito@code0.tech>
2026-06-22 02:30:08 +0200

    feat: interface design for flow compare store

--- src/packages/ce/src/flow/hooks/Flow.compare.hook.ts
+++ src/packages/ce/src/flow/hooks/Flow.compare.hook.ts
@@ -1,9 +1,13 @@
 import { create } from 'zustand'
 import {FlowView} from "@edition/flow/services/Flow.view";
 
-export const useFlowCompareStore = create<{
+interface FlowCompareState {
     flow: FlowView | null
-}>((setState) => ({
+    setFlow: (flow: FlowView) => void
+    clearFlow: () => void
+}
+
+export const useFlowCompareStore = create<FlowCompareState>((setState) => ({
     flow: null,
     setFlow: (flow: FlowView) => setState({flow}),
     clearFlow: () => setState({flow: null}),
