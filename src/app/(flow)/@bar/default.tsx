6e5e25e9e86b5f63b67cc3e05040776c10433a27
nicosammito <nsammito@code0.tech>
2026-07-09 17:57:46 +0200

    feat: simplify default export by directly exporting ApplicationBarView

--- src/app/(flow)/@bar/default.tsx
+++ src/app/(flow)/@bar/default.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import React from "react";
 import {ApplicationBarView} from "@edition/application/views/ApplicationBarView";
 
-const Page = () => {
-    return <ApplicationBarView/>
-}
-
-export default Page
+export default ApplicationBarView
