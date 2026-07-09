507d9959cb3675306b57e4828584f239dc3daa53
nicosammito <nsammito@code0.tech>
2026-07-09 19:26:47 +0200

    feat: remove redundant header from OrganizationsTopView for cleaner UI

--- src/packages/ce/src/organization/views/OrganizationsTopView.tsx
+++ src/packages/ce/src/organization/views/OrganizationsTopView.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import {Flex, Spacing, Text} from "@code0-tech/pictor";
 import React from "react";
 import {useRouter} from "next/navigation";
 import {OrganizationDataTableComponent} from "@edition/organization/components/OrganizationDataTableComponent";
@@ -10,16 +9,6 @@ export const OrganizationsTopView = () => {
     const router = useRouter()
 
     return <>
-        <Flex style={{gap: "0.35rem", flexDirection: "column"}}>
-            <Text size={"xl"} hierarchy={"primary"}>
-                Top organizations
-            </Text>
-            <Text size={"sm"} hierarchy={"tertiary"}>
-                Manage organizations that you belong to. You can create new organizations and switch between them.
-            </Text>
-        </Flex>
-        <Spacing spacing={"xl"}/>
-        {/**TODO: use namespaceId*/}
         <OrganizationDataTableComponent minimized sort={{
             "updatedAt": "desc"
         }} preFilter={(_, index) => index < 5} onSelect={(organization) => {
