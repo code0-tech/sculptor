9bf654ed55333695fda419e58d64ebac5a8f3fcd
nicosammito <nsammito@code0.tech>
2026-07-21 23:44:35 +0200

    feat: refactor ApplicationLayout to use sidebar and FullScreen component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -2,10 +2,10 @@
 
 import React from "react";
 import {useApolloClient} from "@apollo/client/react";
-import {AuroraBackground, ContextStoreProvider, Flex} from "@code0-tech/pictor";
+import {ContextStoreProvider} from "@code0-tech/pictor";
 import {UserService} from "@edition/user/services/User.service";
 import {GraphqlClient} from "@core/util/graphql-client";
-import {usePathname} from "next/navigation";
+import {useParams, usePathname} from "next/navigation";
 import {OrganizationService} from "@edition/organization/services/Organization.service";
 import {MemberService} from "@edition/member/services/Member.service";
 import {NamespaceService} from "@edition/namespace/services/Namespace.service";
@@ -13,30 +13,53 @@ import {usePersistentReactiveArrayService} from "@/hooks/usePersistentReactiveAr
 import {RuntimeService} from "@edition/runtime/services/Runtime.service";
 import {ProjectService} from "@edition/project/services/Project.service";
 import {RoleService} from "@edition/role/services/Role.service";
-import Image from "next/image";
 import {Application, ApplicationService} from "@edition/application/services/Application.service";
 import {useUserSession} from "@edition/user/hooks/User.session.hook";
 import {OrganizationView} from "@edition/organization/services/Organization.view";
 import {RoleView} from "@edition/role/services/Role.view";
 import {Layout} from "@code0-tech/pictor/dist/components/layout/Layout";
-import {Namespace, NamespaceMember, NamespaceProject, Runtime, User} from "@code0-tech/sagittarius-graphql-types";
+import {
+    DataType,
+    Flow, FlowType,
+    FunctionDefinition,
+    Namespace,
+    NamespaceMember,
+    NamespaceProject,
+    Runtime, RuntimeModule,
+    User
+} from "@code0-tech/sagittarius-graphql-types";
 import {ApplicationMiddlewareComponent} from "@edition/application/components/ApplicationMiddlewareComponent";
+import {FullScreen} from "@code0-tech/pictor/dist/components/fullscreen/FullScreen";
+import {ApplicationNavigationView} from "@edition/application/views/ApplicationNavigationView";
+import {FlowService} from "@edition/flow/services/Flow.service";
+import {FunctionService} from "@edition/function/services/Function.service";
+import {DatatypeService} from "@edition/datatype/services/Datatype.service";
+import {FlowTypeService} from "@edition/flowtype/services/FlowType.service";
+import {ModuleService} from "@edition/module/services/Module.service";
+import {AIService, Model} from "@edition/ai/services/AI.service";
 
 interface ApplicationLayoutProps {
     children: React.ReactNode
-    bar: React.ReactNode
-    tab: React.ReactNode
+    sidebar: React.ReactNode
     modal: React.ReactNode
 }
 
-const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, bar, tab, modal}) => {
+const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, sidebar, modal}) => {
 
     const client = useApolloClient()
     const pathname = usePathname()
+    const params = useParams()
     const currentSession = useUserSession()
 
     const graphqlClient = React.useMemo(() => new GraphqlClient(client), [client])
 
+    const namespaceIndex = params.namespaceId as any as number
+    const projectIndex = params.projectId as any as number
+    const flowIndex = params.flowId as any as number
+    const namespaceId: Namespace['id'] = `gid://sagittarius/Namespace/${namespaceIndex}`
+    const projectId: NamespaceProject['id'] = `gid://sagittarius/NamespaceProject/${projectIndex}`
+
+    const application = usePersistentReactiveArrayService<Application, ApplicationService>(`dashboard::application::${currentSession?.id}`, (store) => new ApplicationService(graphqlClient, store))
     const user = usePersistentReactiveArrayService<User, UserService>(`dashboard::users::${currentSession?.id}`, (store) => new UserService(graphqlClient, store))
     const organization = usePersistentReactiveArrayService<OrganizationView, OrganizationService>(`dashboard::organizations::${currentSession?.id}`, (store) => new OrganizationService(graphqlClient, store))
     const member = usePersistentReactiveArrayService<NamespaceMember, MemberService>(`dashboard::members::${currentSession?.id}`, (store) => new MemberService(graphqlClient, store))
@@ -44,54 +67,40 @@ const ApplicationLayout: React.FC<ApplicationLayoutProps> = ({children, bar, tab
     const runtime = usePersistentReactiveArrayService<Runtime, RuntimeService>(`dashboard::global_runtimes::${currentSession?.id}`, (store) => new RuntimeService(graphqlClient, store))
     const project = usePersistentReactiveArrayService<NamespaceProject, ProjectService>(`dashboard::projects::${currentSession?.id}`, (store) => new ProjectService(graphqlClient, store))
     const role = usePersistentReactiveArrayService<RoleView, RoleService>(`dashboard::roles::${currentSession?.id}`, (store) => new RoleService(graphqlClient, store))
-    const application = usePersistentReactiveArrayService<Application, ApplicationService>(`dashboard::application::${currentSession?.id}`, (store) => new ApplicationService(graphqlClient, store))
+    const flow = usePersistentReactiveArrayService<Flow, FlowService>(`dashboard::flows::${currentSession?.id}`, (store) => new FlowService(graphqlClient, store))
+    const functions = usePersistentReactiveArrayService<FunctionDefinition, FunctionService>(`dashboard::functions::${currentSession?.id}`, (store) => new FunctionService(graphqlClient, store))
+    const datatype = usePersistentReactiveArrayService<DataType, DatatypeService>(`dashboard::datatypes::${currentSession?.id}`, (store) => new DatatypeService(graphqlClient, store))
+    const flowtype = usePersistentReactiveArrayService<FlowType, FlowTypeService>(`dashboard::flowtypes::${currentSession?.id}`, (store) => new FlowTypeService(graphqlClient, store))
+    const module = usePersistentReactiveArrayService<RuntimeModule, ModuleService>(`dashboard::modules::${currentSession?.id}`, (store) => new ModuleService(graphqlClient, store))
+    const ai = usePersistentReactiveArrayService<Model, AIService>(`dashboard::ai::${currentSession?.id}`, (store) => new AIService(graphqlClient, store))
+
+    const runtimeId = React.useMemo(() => project[1].getById(projectId, {namespaceId})?.primaryRuntime?.id, [projectId, project[0], namespaceId])
+
+    React.useEffect(() => {
+        if (!currentSession || !namespaceId || !projectId) return
+
+        flow[1].values({namespaceId, projectId})
+        functions[1].values({namespaceId, projectId, runtimeId})
+        datatype[1].values({namespaceId, projectId, runtimeId})
+        flowtype[1].values({namespaceId, projectId, runtimeId})
+        module[1].values({namespaceId, projectId, runtimeId})
+    }, [runtimeId, namespaceId, projectId, currentSession, flow, functions, datatype, flowtype])
 
     const isAddLicensePage = pathname === '/licenses/add';
 
     return <ContextStoreProvider
-        services={[user, organization, member, namespace, runtime, project, role, application]}>
+        services={[application, user, organization, member, namespace, runtime, project, role, flow, functions, datatype, flowtype, module, ai]}>
         <ApplicationMiddlewareComponent>
-            <Layout style={{zIndex: 0}} layoutGap={"0"} showLayoutSplitter={false} leftContent={
-                <Flex p={0.7} pt={1} align={"center"} style={{flexDirection: "column", gap: "0.7rem"}}>
-                    {isAddLicensePage ? (
-                        <AuroraBackground/>
-                    ) : (
-                        <div style={{
-                            position: "absolute",
-                            top: 0,
-                            left: 0,
-                            width: "50%",
-                            transform: "scaleX(-1)",
-                            height: "40%",
-                            zIndex: "-1",
-                        }}>
-                            <div style={{
-                                position: "absolute",
-                                top: "0",
-                                left: "0",
-                                width: "100%",
-                                height: "100%",
-                                background: "radial-gradient(circle at top right,rgba(25, 24, 37, 0.25) 0%, rgba(25, 24, 37, 1) 25%)",
-                                zIndex: "1"
-                            }}/>
-                            <AuroraBackground/>
-
-                        </div>
-                    )}
-                    <Image src={"/CodeZero_Logo.png"} alt={"CodeZero Banner"} width={160} height={0}
-                           style={{width: '38px', height: 'auto'}}/>
-                    {tab}
-                </Flex>
-            }>
-                <Layout showLayoutSplitter={false} pr={0.7} layoutGap={"0"} topContent={<>{bar}</>}>
-                    <>
-                        <Layout>
-                            <>{children}</>
-                        </Layout>
-                        {modal}
-                    </>
+            <FullScreen bg={"var(--secondary)"}>
+                <Layout p={1} showLayoutSplitter={false} layoutGap={16} leftContent={<ApplicationNavigationView/>}>
+                    <Layout showLayoutSplitter={false} layoutGap={16} leftContent={<>{sidebar}</>}>
+                        <>
+                            {children}
+                            {modal}
+                        </>
+                    </Layout>
                 </Layout>
-            </Layout>
+            </FullScreen>
         </ApplicationMiddlewareComponent>
     </ContextStoreProvider>
 }
