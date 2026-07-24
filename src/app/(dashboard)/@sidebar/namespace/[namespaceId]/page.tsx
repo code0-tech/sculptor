c98f0999987f637155f90214fa0ab634c89cf1d7
nicosammito <nsammito@code0.tech>
2026-07-24 22:56:18 +0200

    feat: add NamespaceMenuView to the layout of the main page component

--- src/app/(dashboard)/@sidebar/namespace/[namespaceId]/page.tsx
+++ src/app/(dashboard)/@sidebar/namespace/[namespaceId]/page.tsx
@@ -2,6 +2,7 @@
 
 import {ProjectListView} from "@edition/project/views/ProjectListView";
 import {FlowRecentListView} from "@edition/flow/views/FlowRecentListView";
+import {NamespaceMenuView} from "@edition/namespace/views/NamespaceMenuView";
 import {Flex} from "@code0-tech/pictor";
 
 export default () => {
@@ -9,5 +10,8 @@ export default () => {
         <ProjectListView/>
         <div style={{borderTop: "1px dashed rgba(255,255,255, .1)"}}/>
         <FlowRecentListView/>
+        <div style={{marginTop: "auto"}}>
+            <NamespaceMenuView/>
+        </div>
     </Flex>
 }
