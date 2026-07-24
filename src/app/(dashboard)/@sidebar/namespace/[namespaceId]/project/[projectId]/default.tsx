b39dc453798fcd0a2c2e03d2ebae2e38ef24ad2b
nicosammito <nsammito@code0.tech>
2026-07-24 22:56:10 +0200

    feat: integrate NamespaceMenuView into FlowFolderView layout

--- src/app/(dashboard)/@sidebar/namespace/[namespaceId]/project/[projectId]/default.tsx
+++ src/app/(dashboard)/@sidebar/namespace/[namespaceId]/project/[projectId]/default.tsx
@@ -1,5 +1,14 @@
 "use client"
 
+import {Flex} from "@code0-tech/pictor";
 import {FlowFolderView} from "@edition/flow/views/FlowFolderView";
+import {NamespaceMenuView} from "@edition/namespace/views/NamespaceMenuView";
 
-export default FlowFolderView
+export default () => {
+    return <Flex h={"100%"} style={{boxSizing: "border-box", flexDirection: 'column'}}>
+        <FlowFolderView/>
+        <div style={{marginTop: "auto"}}>
+            <NamespaceMenuView/>
+        </div>
+    </Flex>
+}
