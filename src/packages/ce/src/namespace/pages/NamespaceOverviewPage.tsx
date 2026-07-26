c9c329b9b9d2df76064ed925a868938fcd265e22
nicosammito <nsammito@code0.tech>
2026-07-26 21:21:17 +0200

    feat: enhance NamespaceOverviewPage to include stats and project components

--- src/packages/ce/src/namespace/pages/NamespaceOverviewPage.tsx
+++ src/packages/ce/src/namespace/pages/NamespaceOverviewPage.tsx
@@ -6,10 +6,20 @@ import {
     ScrollArea,
     ScrollAreaScrollbar,
     ScrollAreaThumb,
-    ScrollAreaViewport
+    ScrollAreaViewport,
+    Spacing
 } from "@code0-tech/pictor";
+import {useParams} from "next/navigation";
+import {Namespace} from "@code0-tech/sagittarius-graphql-types";
+import {NamespaceStatsView} from "@edition/namespace/views/NamespaceStatsView";
+import {ProjectTableView} from "@edition/project/views/ProjectTableView";
+import {ApplicationAttentionFlowsComponent} from "@edition/application/components/ApplicationAttentionFlowsComponent";
 
 export const NamespaceOverviewPage: React.FC = () => {
+
+    const params = useParams()
+    const namespaceIndex = params.namespaceId as any as number
+    const namespaceId: Namespace['id'] = `gid://sagittarius/Namespace/${namespaceIndex}`
 
     return <div style={{
         background: "var(--primary)",
@@ -23,6 +33,11 @@ export const NamespaceOverviewPage: React.FC = () => {
         <ScrollArea h={"100%"} type={"scroll"}>
             <ScrollAreaViewport>
                 <div style={{maxWidth: "52rem", margin: "0 auto", padding: "4rem 1rem"}}>
+                    <NamespaceStatsView/>
+                    <Spacing spacing={"xl"}/>
+                    <ApplicationAttentionFlowsComponent namespaceId={namespaceId}/>
+                    <Spacing spacing={"xl"}/>
+                    <ProjectTableView/>
                 </div>
             </ScrollAreaViewport>
             <ScrollAreaScrollbar orientation={"vertical"}>
