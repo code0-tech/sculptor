b179ad57bee3eccb286729162d12e81853577466
nicosammito <nsammito@code0.tech>
2026-07-27 14:36:03 +0200

    feat: remove AuroraBackground component from ApplicationPage for cleaner layout

--- src/packages/ce/src/application/pages/ApplicationPage.tsx
+++ src/packages/ce/src/application/pages/ApplicationPage.tsx
@@ -20,7 +20,6 @@ export const ApplicationPage = () => {
         borderRadius: "1rem",
         padding: "1rem",
     }}>
-        <AuroraBackground/>
         <ScrollArea h={"100%"} type={"scroll"}>
             <ScrollAreaViewport>
                 <div style={{maxWidth: "52rem", margin: "0 auto", padding: "4rem 1rem"}}>
