20179d4a0af7f111f1e8f083d6a9761ec561f5a1
nicosammito <nsammito@code0.tech>
2026-08-20 01:12:17 +0200

    feat: corrct handling of iFrame support for /playground

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,12 @@ import type {NextConfig} from "next";
 import path from "node:path";
 
 const EDITION = process.env.EDITION ?? "ce";
-const IS_PLAYGROUND = (process.env.NEXT_PUBLIC_EDITION ?? EDITION) === "playground";
 const SAGITTARIUS_GRAPHQL_URL = process.env.SAGITTARIUS_GRAPHQL_URL ?? 'http://localhost:3010/graphql';
 const SAGITTARIUS_CABLE_URL = process.env.SAGITTARIUS_CABLE_URL ?? 'http://localhost:3010/cable';
 
-const frameAncestors = IS_PLAYGROUND ? (process.env.PLAYGROUND_FRAME_ANCESTORS ?? "'self'") : "'none'";
+const playgroundFrameAncestors = process.env.PLAYGROUND_FRAME_ANCESTORS ?? "'self'";
 
-const cspHeader = `
+const csp = (frameAncestors: string) => `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
@@ -20,23 +19,32 @@ const cspHeader = `
     frame-ancestors ${frameAncestors};
     worker-src 'self' blob: data: *;
     connect-src 'self' ${SAGITTARIUS_GRAPHQL_URL} ${SAGITTARIUS_CABLE_URL.replace("http", "ws")} ${process.env.NEXT_PUBLIC_OTEL_LOGS_ENDPOINT} ${process.env.NEXT_PUBLIC_OTEL_TRACES_ENDPOINT};
-`
+`.replace(/\n/g, '')
 
 const nextConfig: NextConfig = {
     output: "standalone",
     async headers() {
         return [
             {
-                source: '/(.*)',
+                source: '/playground/:path*',
                 headers: [
                     {
                         key: 'Content-Security-Policy',
-                        value: cspHeader.replace(/\n/g, ''),
+                        value: csp(playgroundFrameAncestors),
                     },
-                    ...(IS_PLAYGROUND ? [] : [{
+                ],
+            },
+            {
+                source: '/((?!playground(?:/|$)).*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: csp("'none'"),
+                    },
+                    {
                         key: "X-Frame-Options",
                         value: "DENY"
-                    }])
+                    }
                 ],
             },
         ]
